88b82a12e7cfa2fcd772673ba439b3315acf2805
foobar0112 <foobar2@posteo.de>
2018-06-18 14:03:34 +0200

    add link to source

--- public/components/footer.php
+++ public/components/footer.php
@@ -5,4 +5,5 @@
   <a href="/impressum">Impressum</a>
 </p>
 <p class="funding-note">Via Förderung von Projekten einzelner Trägerkreisorganisationen wird diese Konferenz vom <a href="https://www.bmbf.de/" target="_blank">Bundesministerium für Bildung und Forschung (BMBF)</a> kofinanziert.<p>
-<p class="funding-note">Wer hat die Website gemacht? <a href="https://kollektiv.afeefa.de" target="_blank">Afeefa Kollektiv</a>.<p>
+<p class="funding-note">Konzept und Umsetzung: <a href="https://kollektiv.afeefa.de" target="_blank">Afeefa Kollektiv</a>
+    <br><a href="https://github.com/afeefade/bitbaum" target="_blank">Quellcode auf GitHub.com</a><p>
