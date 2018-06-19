0be8e7e40074560ec1b09f661430888bc805a524
foobar0112 <foobar2@posteo.de>
2018-06-20 00:41:40 +0200

    update CfP pf, minor changes in footer

--- public/components/footer.php
+++ public/components/footer.php
@@ -4,11 +4,9 @@
     <a href="/datenschutz">Datenschutzerklärung</a>
     <a href="/impressum">Impressum</a>
 </p>
-<p class="funding-note">Via Förderung von Projekten einzelner Trägerkreisorganisationen wird diese Konferenz vom <a
-            href="https://www.bmbf.de/" target="_blank">Bundesministerium für Bildung und Forschung (BMBF)</a>
-    kofinanziert.<p>
 <p class="funding-note">Die Veranstaltung wird gefördert durch die Deutsche Bundesstiftung Umwelt und via Förderung von Projekten
-    einzelner Trägerkreisorganisationen vom Bundesministerium für Bildung und Forschung (BMBF) kofinanziert.</p>
+    einzelner Trägerkreisorganisationen vom <a
+            href="https://www.bmbf.de/" target="_blank">Bundesministerium für Bildung und Forschung (BMBF)</a> kofinanziert.</p>
 <p class="funding-note">Konzept und Umsetzung: <a href="https://kollektiv.afeefa.de" target="_blank">Afeefa
         Kollektiv</a>
     <br><a href="https://github.com/afeefade/bitbaum" target="_blank">Quellcode auf GitHub.com</a><p>
