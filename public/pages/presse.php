4787dfa2cb96e39d4d13ba06cc9ca895bfc9785d
Felix <fische@courages.net>
2018-08-14 17:51:26 +0200

    added press page

---
+++ public/pages/presse.php
@@ -0,0 +1,34 @@
+<?php switch ($lang) {
+    case "en":
+        ?>
+        <article>
+            <h1>Press</h1>
+            <div class="row">
+                <section>
+                    <img class="larger gray" src="/img/nina-rehm.png">
+                    <p>Nina Prehm<br>+49 (0) 30 884594 16<br>kommunikation@ioew.de</p>
+                </section>
+                <section>
+                    <h3>Press releases</h3>
+                    <p>...</p>
+                </section>
+            </div>
+        </article>
+        <?php
+        break;
+    default:
+        ?>
+        <article>
+            <h1>Pressekontakt</h1>
+            <div class="row">
+                <section>
+                    <img class="larger gray" src="/img/nina-rehm.png">
+                    <p>Nina Prehm<br>+49 (0) 30 884594 16<br>kommunikation@ioew.de</p>
+                </section>
+                <section>
+                    <h3>Pressemitteilungen</h3>
+                    <p>...</p>
+                </section>
+            </div>
+        </article>
+    <?php } ?>
