8f44bf7e171713ae1e21fdbeba039aa19ff510c1
Felix <fische@courages.net>
2018-09-14 18:05:31 +0200

    add contact info to footer

---
+++ public/components/contact.php
@@ -0,0 +1,15 @@
+<div class="contact">
+    
+    <?php switch ($lang) {
+        case "en":
+            ?>
+            <p><strong>Contact</strong></p>
+            <?php
+            break;
+        default:
+            ?>
+            <p><strong>Kontakt</strong></p>
+        <?php } ?>
+    
+  <span class="mail_replace_info"></span><br>01753540452
+</div>
