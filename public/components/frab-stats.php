2710c5cd6787da59dde8c0d5cb5805bf63cad7bc
Felix <fische@courages.net>
2018-09-25 21:37:36 +0200

    prepare frab stats in sidebar, but hide for now

---
+++ public/components/frab-stats.php
@@ -0,0 +1,14 @@
+<?php
+require_once('FrabFeed.php');
+
+$feed = new FrabFeed();
+$feed->fetch();
+
+$numberOfSlots = count($feed->getSlots());
+?>
+
+<div>
+  <!-- <p>33 Speakers</p> -->
+  <!-- <p><?php echo $numberOfSlots ?> Slots</p> -->
+  <!-- <p>127 Teilnehmer</p> -->
+</div>
