6e3f8705a09ca13fff3d4578223698037c0b6797
laryllian <748785+laryllian@users.noreply.github.com>
2018-09-26 11:01:01 +0200

    Correct emailaddress

--- public/pages/presse.php
+++ public/pages/presse.php
@@ -7,7 +7,7 @@
                     <h3>Contact</h3>
                     <p>Here you will soon find more information, press releases and download material regarding the conference.</p>
                     <img class="portrait" src="/img/nina-rehm.png">
-                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><a href="mailto:kommunikation@ioew.de">kommunikation@ioew.de</a></p>
+                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><a href="mailto:presse@bits-und-baeume.org">presse@bits-und-baeume.org</a></p>
                     <p>If you would like to receive press releases and information about the conference, please send an e-mail.</p>
                 </section>
                 <!-- <section>
@@ -25,7 +25,7 @@
                 <section>
                     <h3>Pressekontakt</h3>
                     <img class="portrait" src="/img/nina-rehm.png">
-                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><a href="mailto:kommunikation@ioew.de">kommunikation@ioew.de</a></p>
+                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><a href="mailto:presse@bits-und-baeume.org">presse@bits-und-baeume.org</a></p>
                     <p>Wenn Sie in den Presseverteiler aufgenommen werden wollen, schreiben Sie bitte eine E-Mail</p>
                 </section>
                 <!-- <section>
