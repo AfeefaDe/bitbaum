7d03a01955fe5f47a3ed819d2273b79f07ac4ee5
Felix <fische@courages.net>
2018-09-30 14:44:11 +0200

    add PMs and press graphics

--- public/pages/presse.php
+++ public/pages/presse.php
@@ -3,35 +3,58 @@
         ?>
         <article>
             <h1>Press</h1>
-                <section>
-                    <h3>Contact</h3>
-                    <p>Here you will soon find more information, press releases and download material regarding the conference.</p>
-                    <img class="portrait" src="/img/nina-rehm.png">
-                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
-                    <p>If you would like to receive press releases and information about the conference, please send an e-mail.</p>
-                </section>
-                <!-- <section>
-                    <h3>Press releases</h3>
-                    <p>...</p>
-                </section> -->
+            <div class="row">
+              <section>
+                  <h2>Contact</h2>
+                  <p>Here you will soon find more information, press releases and download material regarding the conference.</p>
+                  <img class="portrait" src="/img/nina-rehm.png">
+                  <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
+                  <p>If you would like to receive press releases and information about the conference, please send an e-mail.</p>
+              </section>
+              <section>
+                <h2>Press releases</h2>
+                <p>
+                  <strong>Digitalisierung trifft Nachhaltigkeit: Anmeldestart für die Vernetzungskonferenz Bits & Bäume</strong>
+                  <br>Pressemitteilung vom 01. Oktober 2018
+                  <br>🗎 <a href="/downloads/Digitalisierung-trifft-Nachhaltigkeit-Anmeldestart-für-die-Vernetzungskonferenz-Bits-und-Bäume.pdf">PDF herunterladen</a>
+                </p>
+              </section>
+            </div>
         </article>
         <?php
         break;
     default:
         ?>
         <article>
-            <h1>Presse</h1>
-                <p>Hier finden Sie in Kürze umfangreiche Informationen, Pressemitteilungen und Downloadmaterial zur Konferenz.</p>
-                <section>
-                    <h3>Pressekontakt</h3>
-                    <img class="portrait" src="/img/nina-rehm.png">
-                    <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
-                    <p>Wenn Sie in den Presseverteiler aufgenommen werden wollen, schreiben Sie bitte eine E-Mail</p>
-                </section>
-                <!-- <section>
-                    <h3>Pressemitteilungen</h3>
-                    <p>...</p>
-                </section> -->
-            </div>
+          <h1>Presse</h1>
+          <div class="row">
+            
+            <section>
+              <h2>Pressekontakt</h2>
+              <img class="portrait" src="/img/nina-rehm.png">
+              <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
+              <p>Wenn Sie in den Presseverteiler aufgenommen werden wollen, schreiben Sie bitte eine E-Mail</p>
+            </section>
+            
+            <section>
+              <h2>Pressemitteilungen</h2>
+              <p>
+                <strong>Digitalisierung trifft Nachhaltigkeit: Anmeldestart für die Vernetzungskonferenz Bits & Bäume</strong>
+                <br>Pressemitteilung vom 01. Oktober 2018
+                <br>🗎 <a href="/downloads/Digitalisierung-trifft-Nachhaltigkeit-Anmeldestart-für-die-Vernetzungskonferenz-Bits-und-Bäume.pdf">PDF herunterladen</a>
+              </p>
+              
+              <h2>ÖA-Material</h2>
+              <p>
+                <strong>Flyer zur Bewerbung des Anmeldestarts</strong>
+                <br>🗋 <a target="_blank" href="/downloads/flyer-anmeldestart-2.pdf">Variation 1</a>
+                <br>🗋 <a target="_blank" href="/downloads/flyer-anmeldestart-3.pdf">Variation 2</a>
+                <br>🗋 <a target="_blank" href="/downloads/flyer-anmeldestart-4.pdf">Variation 3</a>
+                <br>🗋 <a target="_blank" href="/downloads/flyer-anmeldestart-5.pdf">Variation 4</a>
+                <br>🗋 <a target="_blank" href="/downloads/flyer-anmeldestart-7.pdf">Variation 5</a>
+              </p>
+            
+            </section>
+          </div>
         </article>
     <?php } ?>
