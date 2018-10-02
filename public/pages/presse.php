fe664255e5f522a32555fdbe9036f11a86452e1a
laryllian <748785+laryllian@users.noreply.github.com>
2018-10-02 14:27:15 +0300

    Change to use smaller person pic

--- public/pages/presse.php
+++ public/pages/presse.php
@@ -7,7 +7,7 @@
               <section>
                   <h2>Contact</h2>
                   <p>Here you will soon find more information, press releases and download material regarding the conference.</p>
-                  <img class="portrait" src="/img/nina-rehm.png">
+                  <img class="portrait" src="/img/nina-rehm.jpg">
                   <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
                   <p>If you would like to receive press releases and information about the conference, please send an e-mail.</p>
               </section>
@@ -23,7 +23,7 @@
             
             <section>
               <h2>Pressekontakt</h2>
-              <img class="portrait" src="/img/nina-rehm.png">
+              <img class="portrait" src="/img/nina-rehm.jpg">
               <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
               <p>Wenn Sie in den Presseverteiler aufgenommen werden wollen, schreiben Sie bitte eine E-Mail</p>
             </section>
