b4152415ff2ce2f5e66f9fec4c489d32f9a4217d
yuelika <32544557+yuelika@users.noreply.github.com>
2018-10-31 14:04:00 +0100

    add livestream info de/en, add translation info en

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -7,7 +7,7 @@
         </section>
 
         <section class="switch program active">
-          <p>The program with more than 120 international panels, talks, workshops, philosophical salon, forum, sporangium, concert and party is now online. Numerous organizations, individuals and international speakers are shaping the program with their submissions and presenting their approaches to sustainable digitization. Together with you we want to develop solutions for a sustainable future and change the world.</p>
+          <p>The program with more than 120 international panels, talks, workshops, forum, philosophical salon, sporangium, concert and party is now online. Numerous organizations, individuals and international speakers are shaping the program with their submissions and presenting their approaches to sustainable digitization. Together with you we want to develop solutions for a sustainable future and change the world. For those who can't join us in Berlin: There wil be a livestream of all talks, panels and discussions on our five stages. All panels on the Bits&Bäume stage will be translated into English. </p>
           <a href="https://fahrplan.bits-und-baeume.org" target="_blank" class="button" style="text-align: center">See full programme</a>
         </section>
 
@@ -36,7 +36,7 @@ break;
 
         <section class="switch program active">
           <h4>PROGRAMM</h4>
-          <p>Das Programm mit mehr als 120 international besetzten Panels, Talks, Workshops, Philosophischem Salon, Forum, Sporangium, Konzert und 	Party ist jetzt online. Zahlreiche Organisationen, Personen und internationale Referierende gestalten das Programm durch ihre Einreichungen mit und stellen ihre Ansätze für eine nachhaltige Digitalisierung vor. Gemeinsam mit euch wollen wir Lösungsansätze für eine nachhaltige Zukunft erarbeiten und die Welt verändern.</p>
+          <p>Das Programm mit mehr als 120 international besetzten Panels, Talks, Workshops, Forum, Philosophischem Salon, Sporangium, Konzert und	Party ist jetzt online. Zahlreiche Organisationen, Personen und internationale Referierende gestalten das Programm durch ihre Einreichungen mit und stellen ihre Ansätze für eine nachhaltige Digitalisierung vor. Gemeinsam mit euch wollen wir Lösungsideen für eine nachhaltige Zukunft erarbeiten und die Welt verändern. Wer nicht kommen kann, für den gibt es den Livestream unserer 5 Bühnen.</p>
           <a href="https://fahrplan.bits-und-baeume.org" target="_blank" class="button" style="text-align: center">Komplettes Programm ansehen</a>
         </section>
 
