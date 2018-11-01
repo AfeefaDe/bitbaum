579ddaeeb5b8b39736f53a0348801edd566fc49d
Felix <fische@courages.net>
2018-11-01 22:08:21 +0100

    remove a piece of text

--- public/pages/unterstuetzen.php
+++ public/pages/unterstuetzen.php
@@ -5,7 +5,7 @@
             <h1>Helfer*innen für die Konferenz <em>Bits & Bäume</em> gesucht</h1>
             <div class="row">
                 <section>
-                    <p>Auf der Bits & Bäume erwarten wir über 1.000 Teilnehmer*innen. Um eine so große Konferenz zu stemmen, sind wir auf die Unterstützung von freiwilligen Helfer*innen angewiesen. 
+                    <p>Auf der Bits & Bäume erwarten wir über 1.000 Teilnehmer*innen. Um eine so große Konferenz zu stemmen, sind wir auf die Unterstützung von freiwilligen Helfer*innen angewiesen.
 Ihr erhaltet an beiden Tagen freien Eintritt zur Konferenz und werdet mit bio-veganem Essen von der Fläming Kitchen verpflegt. Mitzuhelfen bedeutet vor allem mit vielen neuen Leuten zu schnacken, euch mit neuen Communities zu vernetzen und euch ganz praktisch für eine nachhaltige Digitalisierung einzusetzen.</p>
                 </section>
                 <section>
@@ -20,11 +20,11 @@ Ihr erhaltet an beiden Tagen freien Eintritt zur Konferenz und werdet mit bio-ve
                 </section>
             </div>
             <p>Wir werden versuchen, die Aufgaben gemeinsam mit euch nach jeweiligen Präferenzen zu verteilen. Geplant ist, dass ihr Samstag und Sonntag jeweils 4 Stunden mithelft. Für ein erstes Kennenlernen aller Helfenden und die Verteilung der Schichten planen wir ein gemeinsames <span class="bolder">Vorbereitungstreffen in der Woche vor der Konferenz</span>.</p>
-            <p>Hast du Lust an unserer Konferenz teilzunehmen und uns zu helfen? Dann schreib uns! Melde dich <span class="bolder">bis spätestens zum 26.10.</span> bei: <span class="mail_replace_katja"></p>
+            <p>Hast du Lust an unserer Konferenz teilzunehmen und uns zu helfen? Dann schreib uns! Melde dich bei: <span class="mail_replace_katja"></p>
     <h1>Stage Hosts für die Konferenz <em>Bits & Bäume</em> gesucht</h1>
             <div class="row">
                 <section>
-                    <p>Für die Hauptbühnen von <em>Bits & Bäume</em> sind wir auf der Suche nach Stage Hosts. Als Stage Host begleitest du das Publikum auf einer Bühne durch das Programm.</p>      
+                    <p>Für die Hauptbühnen von <em>Bits & Bäume</em> sind wir auf der Suche nach Stage Hosts. Als Stage Host begleitest du das Publikum auf einer Bühne durch das Programm.</p>
                 </section>
                 <section>
                     <p>Konkrete <strong>Aufgaben</strong> werden unter anderem sein:</p>
@@ -41,6 +41,6 @@ Ihr erhaltet an beiden Tagen freien Eintritt zur Konferenz und werdet mit bio-ve
                 </section>
             </div>
             <p>Hast du bereits Moderationserfahrung und Lust dich aktiv am Programm der Konferenz zu beteiligen? Dann melde dich <span class="bolder">bis zum 08.11. </span>bei <span class="mail_replace_katja">. </p>
-            <p>Bitte beschreibe in der E-Mail deine bisherige Erfahrung und schicke uns Videos zu deinen Moderationen.</p>           
+            <p>Bitte beschreibe in der E-Mail deine bisherige Erfahrung und schicke uns Videos zu deinen Moderationen.</p>
     </article>
-    <?php } ?>
+    <?php }?>
