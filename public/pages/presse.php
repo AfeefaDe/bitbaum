ae875aa7a2e1cba757a062591fc11e7d14390f72
Felix <fische@courages.net>
2018-11-07 21:15:28 +0100

    new content for press page

--- public/pages/presse.php
+++ public/pages/presse.php
@@ -46,18 +46,24 @@ break;
         <article>
           <h1>Presse & Öffentlichkeitsarbeit</h1>
           <div class="row">
-
             <section>
               <h2>Pressekontakt</h2>
               <img class="portrait" src="/img/nina-rehm.jpg">
               <p><strong>Nina Prehm</strong><br>+49 (0) 30 884594 48<br><span class="mail_replace_presse"></span></p>
               <p>Wenn Sie sich zur Konferenz anmelden oder in den Presseverteiler aufgenommen werden wollen, schreiben Sie bitte eine E-Mail.</p>
             </section>
+
             <section>
-            <h2>Social Media</h2>
-            <img class="portrait" src="/img/jasmin-helm.jpg">
-            <p><strong>Jasmin Helm</strong><br><a href="https://twitter.com/bitsundbaeume" target="_blank">@bitsundbaeume</a>
+              <h2>Social Media</h2>
+              <img class="portrait" src="/img/jasmin-helm.jpg">
+              <p><strong>Jasmin Helm</strong><br><a href="https://twitter.com/bitsundbaeume" target="_blank">@bitsundbaeume</a>
             </section>
+
+          </div>
+        </article>
+
+        <article>
+          <div class="row">
             <section>
               <h2>Pressemitteilungen</h2>
               <p>
@@ -70,37 +76,50 @@ break;
                 <br>Pressemitteilung vom 02. Oktober 2018
                 <br>🗎 <a target="_blank" href="/downloads/181002-Bits-und-Baueme-Anmeldestart.pdf">PDF herunterladen</a>
               </p>
+            </section>
 
+            <section>
               <h2>ÖA-Material</h2>
               <p>
                 🗋 <a target="_blank" href="/downloads/bits-und-baeume-logo.pdf">Logo</a>
                 <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-logo-schrift.pdf">Logo mit Schrift</a>
-                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-graphik-traegerkreis.jpg">Grafik mit Trägerkreis-Logos</a>
-                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-graphik-loesungen.png">Grafik „Lösungen“</a>
-                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-graphik-programmstart.png">Grafik Programmstart</a>
+                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-logo-konferenzinfos.png">Grafik mit Konferenzinfos</a>
+                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-logo-programmpunkte.png">Grafik mit Programmpunkten</a>
+                <br>🗋 <a target="_blank" href="/downloads/bits-und-baeume-logo-traegerkreis.png">Grafik mit Trägerkreis</a>
               </p>
-
+              <p>🎬 <a href="https://vimeo.com/user91395992" target="_blank">Kurzinterviews zur Konferenz von Personen aus dem Trägerkreis</a></p>
             </section>
           </div>
         </article>
 
         <article>
-          <h2>FAQ</h2>
+        <div class="row">
+          <section>
+            <h2>FAQ</h2>
 
-          <h3>Wie komme ich als Pressevertreter*in auf die Bits & Bäume?</h3>
-          <p>Es gibt für Pressevertreter*innen die Möglichkeit, sich für die Konferenz akkreditieren zu lassen. Dafür schreibt uns bitte eine Mail an Nina Prehm: <span class="mail_replace_presse"></span>. Wir prüfen alle Anfragen und melden uns schnellstmöglich zurück.</p>
-          <p>Wir freuen uns über Belegexemplare oder Link zum Artikel oder Interview.</p>
+            <h3>Wie komme ich als Pressevertreter*in auf die Bits & Bäume?</h3>
+            <p>Es gibt für Pressevertreter*innen die Möglichkeit, sich für die Konferenz akkreditieren zu lassen. Dafür schreibt uns bitte eine Mail an Nina Prehm: <span class="mail_replace_presse"></span>. Wir prüfen alle Anfragen und melden uns schnellstmöglich zurück.</p>
+            <p>Wir freuen uns über Belegexemplare oder Link zum Artikel oder Interview.</p>
 
-          <h3>Gibt es eine Möglichkeit, Interviews zu führen?</h3>
-          <p>Falls du ein Interview mit einem*r der Speaker*innen oder den Veranstalter*innen führen möchtest, vermitteln wir dir gerne eine*n Ansprechpartner*in. Dafür schreibe uns bitte eine E-Mail an Nina Prehm: <span class="mail_replace_presse"></span> und wir melden uns baldmöglichst bei dir.</p>
-          <p>Auch bei anderen Anliegen bezüglich deiner Berichterstattung melde dich gerne bei uns.</p>
+            <h3>Gibt es eine Möglichkeit, Interviews zu führen?</h3>
+            <p>Falls du ein Interview mit einem*r der Speaker*innen oder den Veranstalter*innen führen möchtest, vermitteln wir dir gerne eine*n Ansprechpartner*in. Dafür schreibe uns bitte eine E-Mail an Nina Prehm: <span class="mail_replace_presse"></span> und wir melden uns baldmöglichst bei dir.</p>
+            <p>Auch bei anderen Anliegen bezüglich deiner Berichterstattung melde dich gerne bei uns.</p>
 
-          <h3>Kann ich die Pressegrafiken und Bilder nutzen?</h3>
-          <p>Die Bilder und Pressegrafiken auf der Website dürfen im Rahmen der Berichterstattung über die Konferenz unter Nennung des Urhebers frei genutzt werden.</p>
+            <h3>Kann ich die Pressegrafiken und Bilder nutzen?</h3>
+            <p>Die Bilder und Pressegrafiken auf der Website dürfen im Rahmen der Berichterstattung über die Konferenz unter Nennung des Urhebers frei genutzt werden.</p>
 
-          <h3>Wo finde ich das Pressematerial und weitere Infos zur Konferenz?</h3>
-          <p>Für allgemeine Informationen über die Konferenz findest du viel Material unter <a href="https://bits-und-baeume.org/presse">www.bits-und-baeume.org/presse</a>. Wir ergänzen die Seite laufend mit Infomaterial. Falls ihr noch Fragen habt oder etwas nicht gefunden habt, schreibt uns gerne eine Mail an <span class="mail_replace_presse"></span></p>
+            <h3>Wo finde ich das Pressematerial und weitere Infos zur Konferenz?</h3>
+            <p>Für allgemeine Informationen über die Konferenz findest du viel Material unter <a href="https://bits-und-baeume.org/presse">www.bits-und-baeume.org/presse</a>. Wir ergänzen die Seite laufend mit Infomaterial. Falls ihr noch Fragen habt oder etwas nicht gefunden habt, schreibt uns gerne eine Mail an <span class="mail_replace_presse"></span></p>
 
-          <p>Ihr findet uns auf Twitter unter <a href="https://twitter.com/bitsundbaeume" target="_blank">@bitsundbaeume</a></p>
+            <p>Ihr findet uns auf Twitter unter <a href="https://twitter.com/bitsundbaeume" target="_blank">@bitsundbaeume</a></p>
+          </section>
+
+          <section>
+            <h2>Weitere Informationen zu den Themen der Konferenz</h2>
+            <p><a href="https://germanwatch.org/de/digitalisierung" target="_blank">Germanwatch über zukunftsfähige Digitalisierung</a></p>
+            <p><a href="https://info.brot-fuer-die-welt.de/blog/sven-hilbig" target="_blank">Blog von Sven Hilbig (Brot für die Welt)</a></p>
+            <p><a href="https://www.ioew.de/digitaler-wandel/" target="_blank">IÖW zum Thema "Digitaler Wandel"</a></p>
+          </section>
+        </div>
         </article>
     <?php }?>
