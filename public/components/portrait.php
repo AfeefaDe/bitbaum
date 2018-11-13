52ace4ee9faa402413dd9f78520e215400045a83
Felix <fische@courages.net>
2018-11-13 08:30:50 +0100

    add vimeo links to portraits

--- public/components/portrait.php
+++ public/components/portrait.php
@@ -2,4 +2,7 @@
     <div class="image" style="background-image: url('<?php echo $human['picture']; ?>')"></div>
     <blockquote><?php echo $human['quote_'.$lang]; ?></blockquote>
     <p><?php echo $human['name'] . ', ' . $human['orga']; ?></p>
+    <?php if (isset($human['vimeo'])) { ?>
+    <p><a href="<?php echo $human['vimeo']; ?>" target="_blank">Kurzinterview</a> mit <?php echo $human['orga']; ?></p>
+    <?php } ?>
 </div>
