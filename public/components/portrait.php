aeaa3f97426541a5dac0f15bc22c02da95169a73
Felix <fische@courages.net>
2018-11-19 12:39:35 +0100

    integrate forderungen into portrait slider

--- public/components/portrait.php
+++ public/components/portrait.php
@@ -1,8 +1,14 @@
 <div class="portrait">
     <div class="image" style="background-image: url('<?php echo $human['picture']; ?>')"></div>
-    <blockquote><?php echo $human['quote_'.$lang]; ?></blockquote>
+    <blockquote><?php echo $human['quote_' . $lang]; ?></blockquote>
+
+    <?php if ($human['name']) {?>
     <p><?php echo $human['name'] . ', ' . $human['orga']; ?></p>
-    <?php if (isset($human['vimeo'])) { ?>
+    <?php } else {?>
+    <p><?php echo $human['orga']; ?></p>
+    <?php }?>
+
+    <?php if (isset($human['vimeo'])) {?>
     <p><a href="<?php echo $human['vimeo']; ?>" target="_blank">Kurzinterview</a> mit <?php echo $human['orga']; ?></p>
-    <?php } ?>
+    <?php }?>
 </div>
