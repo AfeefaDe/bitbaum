52a0b57ee52ae14c4db363b4255f54aff4996c13
laryllian <748785+laryllian@users.noreply.github.com>
2018-11-19 23:12:47 +0100

    Update contact.php

--- public/components/contact.php
+++ public/components/contact.php
@@ -5,7 +5,7 @@
             ?>
             <p><strong>Contact</strong></p>
             <span class="mail_replace_info"></span><br>01753540452<br><br>
-            <p>Bits&Bäume core team: Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Saadya Windauer, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick<br>
+            <p>Bits&Bäume core team: Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick<br>
             Bits&Bäume office: Ekaterina Sergienko, Eva Laderick, Katja George</p>
             <?php
             break;
@@ -13,7 +13,7 @@
             ?>
             <p><strong>Kontakt</strong></p>
             <span class="mail_replace_info"></span><br>01753540452<br><br>
-            <p>Kernteam der Bits&Bäume: Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Saadya Windauer, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick<br>
+            <p>Kernteam der Bits&Bäume: Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick<br>
             Bits&Bäume-Büro: Ekaterina Sergienko, Eva Laderick, Katja George</p>
         <?php } ?>
 
