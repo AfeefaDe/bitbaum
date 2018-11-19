b64680cf390be45091b3b9d18bb51357feaeffae
yuelika <32544557+yuelika@users.noreply.github.com>
2018-11-19 19:47:59 +0100

    add breaks

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -7,7 +7,7 @@
 
         <section class="switch program active">
           <h4>PROGRAMME</h4>
-          <p>The program with more than 120 international panels, talks, workshops, forum, philosophical salon, sporangium, concert and party is now online. Numerous organizations, individuals and international speakers are shaping the program with their submissions and presenting their approaches to sustainable digitization. Together with you we want to develop solutions for a sustainable future and change the world. For those who could not join us in Berlin: All talks, panels and discussions on our five stages were recorded, a translation of all panels on the Bits&Bäume stage ist available. </p>
+          <p>The program with more than 120 international panels, talks, workshops, forum, philosophical salon, sporangium, concert and party is now online. Numerous organizations, individuals and international speakers are shaping the program with their submissions and presenting their approaches to sustainable digitization. Together with you we want to develop solutions for a sustainable future and change the world. <br>For those who could not join us in Berlin: All talks, panels and discussions on our five stages were recorded, a translation of all panels on the Bits&Bäume stage ist available. </p>
           <a href="/programm/<?php echo $lang; ?>" class="button" style="text-align: center">Programme overview</a>
           <p style="display: flex; justify-content: space-evenly;">
             <a href="https://fahrplan.bits-und-baeume.org" target="_blank">See full programme</a>
@@ -42,7 +42,7 @@ break;
         </section>
         <section class="switch program active">
           <h4>PROGRAMM</h4>
-          <p>Das Programm mit mehr als 120 international besetzten Panels, Talks, Workshops, Forum, Philosophischem Salon, Sporangium, Konzert und	Apéro ist jetzt online. Zahlreiche Organisationen, Personen und internationale Referierende gestalten das Programm durch ihre Einreichungen mit und stellen ihre Ansätze für eine nachhaltige Digitalisierung vor. Gemeinsam mit euch wollen wir Lösungsideen für eine nachhaltige Zukunft erarbeiten und die Welt verändern. Wer nicht kommen konnte oder Vortäge, Projektvorstellungen und Diskussionsrunden verpasst hat: Das Programm unserer fünf Bühnen wurde vom Video Operation Center des CCC aufgezeichnet.</p>
+          <p>Das Programm mit mehr als 120 international besetzten Panels, Talks, Workshops, Forum, Philosophischem Salon, Sporangium, Konzert und	Apéro ist jetzt online. Zahlreiche Organisationen, Personen und internationale Referierende gestalten das Programm durch ihre Einreichungen mit und stellen ihre Ansätze für eine nachhaltige Digitalisierung vor. Gemeinsam mit euch wollen wir Lösungsideen für eine nachhaltige Zukunft erarbeiten und die Welt verändern. <br>Wer nicht kommen konnte oder Vortäge, Projektvorstellungen und Diskussionsrunden verpasst hat: Das Programm unserer fünf Bühnen wurde vom Video Operation Center des CCC aufgezeichnet.</p>
           <a href="/programm/<?php echo $lang; ?>" class="button" style="text-align: center">Zur Programmbeschreibung</a>
           <p style="display: flex; justify-content: space-evenly;">
             <a href="https://fahrplan.bits-und-baeume.org" target="_blank">Komplettes Programm ansehen</a>
