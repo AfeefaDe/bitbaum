58c9b0ef075f8cd6f462585edeb5ad30cd0f879c
laryllian <748785+laryllian@users.noreply.github.com>
2019-05-09 22:46:33 +0200

    Update waechst.php

--- public/pages/waechst.php
+++ public/pages/waechst.php
@@ -31,9 +31,9 @@ Not yet!
             <section>
               <h2>Nächste B&B-Veranstaltungen</h2>
 <ul>
-    <li><a href="https://dresden.bits-und-baeume.org">Bits & Bäume Dresden – Eine Abendveranstaltung zu Digitalisierung und Nachhaltigkeit</a><br />
-Zeit: <strong>23. Mai 2019</strong>, ab 18:30 Uhr<br />
-Ort: TU-Dresden, Fakultät Informatik, <a href="https://navigator.tu-dresden.de/etplan/apb/00/raum/542100.2310">Andreas-Pfitzmann-Bau</a>, E023
+    <li><a href="https://dresden.bits-und-baeume.org">Bits & Bäume Dresden – Eine Abendveranstaltung zu Digitalisierung und Nachhaltigkeit</a></li>
+    <li>Zeit: <strong>23. Mai 2019</strong>, ab 18:30 Uhr</li>
+    <li>Ort: TU-Dresden, Fakultät Informatik, <a href="https://navigator.tu-dresden.de/etplan/apb/00/raum/542100.2310">Andreas-Pfitzmann-Bau</a>, E023</li>
     </li>
 </ul>
             </section>
