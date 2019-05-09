c85478f670a8404eb93560bd92a3f221f59aebf0
laryllian <748785+laryllian@users.noreply.github.com>
2019-05-09 22:42:27 +0200

    Update waechst.php

--- public/pages/waechst.php
+++ public/pages/waechst.php
@@ -30,21 +30,30 @@ Not yet!
               <p>Über ein Jahr lang haben wir als Team aus 10 Organisationen daran gearbeitet, die Bits&Bäume 2018 aus der Taufe zu heben – das war sehr bereichernd für uns, aber auch viel Arbeit, gerade weil so unterschiedliche Organisationen an einem Tisch saßen. Wir bekommen viele freudige Hoffnungsbekundungen auf eine Wiederholung 2019, was uns sehr freut, allerdings können wir leider diese gemeinsame Arbeit nicht langfristig in unseren Arbeitsalltag integrieren. Aber die Bits & Bäume war auch eine Konferenz von uns allen für uns alle! Nehmt den Faden also doch einfach auf, gründet einen Stammtisch in eurer Stadt, organisiert eine kleinere oder größere Veranstaltung, einen Diskussionsabend, erklärt auf Panels, warum und wie Digitalisierung und Nachhaltigkeit zusammengehören, lasst uns weiterhin unter #bitsundbäume Ideen und Fortschritte austauschen, denkt euch neue Vernetzungsformate aus.</p>              
             </section>
             <section>
-Was heißt das nun konkret?
+              <h2>Nächste B&B-Veranstaltungen</h2>
 <ul>
-    <li>Inhaltliche Ausrichtung: Ihr könnt den Namen für alle Aktionen weiterverwenden, sofern sie an der Schnittstelle zwischen kritischer Digitalisierung und Nachhaltigkeit liegen. </li>
-    <li>Basisarbeit: Als Beitragende und Teilnehmende wendet ihr euch primär an Aktivist*innen, die wachsende Bits&Bäume-Community, die Wissenschaft und an uns alle als Zivilgesellschaft.</li>
-    <li>Forderungen weitertragen: Bedingung ist, dass ihr euch hinter unsere Forderungen [https://bits-und-baeume.org/forderungen/] stellt und diese bzw. deren Inhalte thematisch wesentlich für eure Aktion sind.</li>
-    <li>Dem eigenen Anspruch folgen: Wir haben versucht, die Konferenz so nachhaltig wie möglich zu gestalten: von veganer und ökologischer Verpflegung über die Zusammenarbeit mit Kollektiven bis hin zur Nutzung freier Lizenzen und Software – das wünschen wir uns auch von allen nachfolgenden Aktionen. Dokumentation, Tipps und Anregungen haben wir hier zusammengetragen [https://bits-und-baeume.org/infrastruktur/]; Ergänzungen und Hinweise erwünscht!</li>
+    <li>Bits & Bäume Dresden<br />
+Eine Abendveranstaltung zu Digitalisierung und Nachhaltigkeit<br />
+Zeit: 23. Mai 2019, ab 18:30 Uhr<br />
+Ort: TU-Dresden, Fakultät Informatik, Andreas-Pfitzmann-Bau, E023
+    </li>
 </ul>
             </section>
           </div>
-
           <section>
-Folgendes legen wir Euch zusätzlich ans Herz:
+              <h2>Was heißt das nun konkret?</h2>
+              <ul>
+                  <li>Inhaltliche Ausrichtung: Ihr könnt den Namen für alle Aktionen weiterverwenden, sofern sie an der Schnittstelle zwischen kritischer Digitalisierung und Nachhaltigkeit liegen. </li>
+                  <li>Basisarbeit: Als Beitragende und Teilnehmende wendet ihr euch primär an Aktivist*innen, die wachsende Bits&Bäume-Community, die Wissenschaft und an uns alle als Zivilgesellschaft.</li>
+                  <li>Forderungen weitertragen: Bedingung ist, dass ihr euch hinter unsere <a href="https://bits-und-baeume.org/forderungen/">Forderungen</a> stellt und diese bzw. deren Inhalte thematisch wesentlich für eure Aktion sind.</li>
+                  <li>Dem eigenen Anspruch folgen: Wir haben versucht, die Konferenz so nachhaltig wie möglich zu gestalten: von veganer und ökologischer Verpflegung über die Zusammenarbeit mit Kollektiven bis hin zur Nutzung freier Lizenzen und Software – das wünschen wir uns auch von allen nachfolgenden Aktionen. Dokumentation, Tipps und Anregungen haben wir <a href="[https://bits-und-baeume.org/infrastruktur/">hier zusammengetragen</a>; Ergänzungen und Hinweise erwünscht!</li>
+              </ul>
+          </section>
+          <section>
+<h2>Folgendes legen wir Euch zusätzlich ans Herz:</h2>
 <ul>
     <li>Beteiligung fördern: Falls ihr eine Konferenz inkl. Vorträgen und Workshops veranstaltet, startet bitte einen Aufruf zur Beteiligung am Programm und verbreitet ihn so weit ihr könnt – denn nur so könnt ihr auch Ideen und Menschen einbeziehen, die ihr noch nicht kennt und es können neue Netzwerke entstehen. </li>
-    <li>Vernetzung ermöglichen: Sorgt auch vor Ort explizit für Zeit und Raum zum Austausch und für Vernetzung. Verwendet gern und viel den Hashtag #bitsundbäume, z.B. auf Mastodon, Twitter oder Diaspora; wir greifen das via @bitsundbaeume gerne auf. </li>
+    <li>Vernetzung ermöglichen: Sorgt auch vor Ort explizit für Zeit und Raum zum Austausch und für Vernetzung. Verwendet gern und viel den Hashtag <a href="https://twitter.com/hashtag/bitsundbaeume">#bitsundbäume</a>, z.B. auf Mastodon, Twitter oder Diaspora; wir greifen das via @bitsundbaeume gerne auf. </li>
     <li>Wenn ihr ein größeres Event plant, richten wir euch bei Bedarf eine Subdomain unter bits-und-baeume.org ein, die dann auf eure Seite zeigt. Diese verlinken wir außerdem prominent auf der B&B-Hauptseite in einer Übersicht der nächsten Aktionen. Bitte denkt an ein eigenes Impressum und eine Datenschutzerklärung.</li>
 </ul>    
           </section>
@@ -53,7 +62,7 @@ Folgendes legen wir Euch zusätzlich ans Herz:
 <p>Wir wollen explizit nicht, dass der Name für Formate verwendet wird, auf denen sich Tech-Giganten einkaufen können und auf denen Politiker*innen Raum bekommen, für sich und ihre Partei zu werben statt ernsthaft zu diskutieren. Diese Gruppen haben genug andere Möglichkeiten, sich Gehör zu verschaffen, sind aber als mitdenkende Besucher*innen freilich stets willkommen. <br />
 Wir wollen mit allen an diesem Ziel arbeiten, die sich mit ganzem Herzen einer für alle lebenswerten materiellen und immateriellen Welt verschrieben haben. </p>
 
-<p>Wenn ihr Anregungen braucht oder hilfreiche Informationen bei uns vermutet, dann meldet euch gerne: Unter bewegung@bits-und-baeume.org erreicht ihr aktuell Juliane (OKF) und Rainer (FIfF). </p>
+<p>Wenn ihr Anregungen braucht oder hilfreiche Informationen bei uns vermutet, dann meldet euch gerne: Unter <a href="mailto:bewegung@bits-und-baeume.org">bewegung@bits-und-baeume.org</a> erreicht ihr aktuell Juliane (OKF) und Rainer (FIfF). </p>
 
 <p>Wir träumen davon, dass der Bits & Bäume 2018 weit mehr Aktionen folgen werden als wir selbst je organisieren könnten! Dezentral und doch verbunden.</p>
 <p>Lasst uns weiter zusammen(-)wachsen!<br />
