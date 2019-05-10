afd2a0d019112f0586f14ad5c89d4679d34aac9d
laryllian <748785+laryllian@users.noreply.github.com>
2019-05-10 17:20:45 +0200

    added english translation

--- public/pages/waechst.php
+++ public/pages/waechst.php
@@ -16,8 +16,53 @@ article .row--forderung > p {
     case "en":
             ?>
 
-Not yet available in English!
+<article>
+          <h1>The Bits & Bäume grows to become a movement!</h1>
+          <div class="row">
+            <section>
+              <p>We, the initiators of the first Bits & Bäume Conference, decided to <strong>release the idea of Bits & Bäume into freedom so that it can continue to grow and prosper.</strong></p>
+              <p>For over a year our team of 10 organizations worked hard to bring the Bits & Bäume 2018 into being - that was very enriching for us, but also a lot of effort, just because so many different organizations were sitting at the same table. We get many joyful expressions of hope for a repeat in 2019, which makes us very happy, but unfortunately we cannot integrate this time intensive work into our daily routines in the long run. But the Bits & Bäume was also a <strong>conference of all of us for all of us!</strong> So just pick up the thread, set up a regular's table in your city, organize a smaller or larger event, a discussion evening, explain on panels why and how digitization and sustainability belong together, let's continue to exchange ideas and successes under <a href="https://twitter.com/hashtag/bitsundbaeume">#bitsundbäume</a> or make up new networking formats.</p>              
+            </section>
+            <section>
+              <h2>Next B&B-Events</h2>
+<ul>
+    <li><a href="https://dresden.bits-und-baeume.org">Bits & Bäume Dresden – Eine Abendveranstaltung zu Digitalisierung und Nachhaltigkeit</a></li>
+    <li>Zeit: <strong>23. Mai 2019</strong>, ab 18:30 Uhr</li>
+    <li>Ort: TU-Dresden, Fakultät Informatik, <a href="https://navigator.tu-dresden.de/etplan/apb/00/raum/542100.2310">Andreas-Pfitzmann-Bau</a>, E023</li>
+    </li>
+</ul>
+            </section>
+          </div>
+          <section>
+              <h2>What does that mean in concrete terms?</h2>
+              <ul>
+                  <li><strong>Topic</strong>: You can use the name for all actions as long as they take place at the interface between critical digitisation and sustainability.</li>
+                  <li><strong>Target group</strong>: As contributors and participants, you primarily address activists, the growing Bits & Bäume community, academia and all of us as civil society.</li>
+                  <li><strong>Support the demands</strong>: Our condition is that you support our demands and that they are thematically essential for your action.</li>
+                  <li><strong>Implement our claims:</strong>:  We have tried to make the conference as sustainable as possible: from vegan and ecological catering and cooperation with collectives to the use of free licenses and software - that's what we want from all subsequent activities. We have compiled <a href="https://bits-und-baeume.org/infrastruktur/">documentation, tips and suggestions here</a>; further additions welcome!</li>
+              </ul>
+          </section>
+          <section>
+<h2>Additional recommendations</h2>
+<ul>
+    <li><strong>Encourage participation</strong>: If you organize a conference including lectures and workshops, please start a call for participation (CfP) and spread it as far as you can - because only in this way you can include ideas and people you don't already know and new networks can develop.</li>
+    <li><strong>Facilitate networking</strong>: Make sure there is time and space for exchange and networking. Use the Hashtag <a href="https://twitter.com/hashtag/bitsundbaeume">#bitsundbäume</a> with pleasure and often, e.g. on Mastodon, Twitter or Diaspora; we'll gladly take this up via @bitsundbaeume.</li>
+    <li>If you're planning a bigger event, we'll set up a <strong>subdomain at bits-und-baeume.org</strong> for you. We will also link it prominently on the B&B main page in an overview of the next actions. Please create your own imprint and privacy policy though.</li>
+</ul>    
+          </section>
 
+          <section>
+<p>We explicitly don't want the name to be used for formats where tech giants can buy their way into it and where politicians can advertise for themselves and their party instead of discussing seriously. These groups have enough possibilities already to make themselves heard, but they are surely always welcome as listening visitors.<br />
+We want to work towards this goal with everyone those who have dedicated themselves wholeheartedly to a material and immaterial world worth living in for all.</p>
+
+<p>If you need any suggestions or suspect helpful information from us, please feel free to contact us: At b<a href="mailto:bewegung@bits-und-baeume.org">bewegung@bits-und-baeume.org</a> you can currently reach Juliane (OKF) and Rainer (FIfF).</p>
+
+<p>We dream that the Bits & Bäume 2018 will be followed by far more actions than we could ever organize ourselves! Decentralized and yet connected.</p>
+<p>Let us continue to grow together!<br />
+Your B&B-Team</p>
+          </section>
+
+        </article>
             <?php
             break;   
     default:
@@ -27,7 +72,7 @@ Not yet available in English!
           <div class="row">
             <section>
               <p>Wir Initiator*innen der ersten Bits&Bäume-Konferenz haben uns entschieden, <strong>die Idee der Bits & Bäume in die Freiheit zu entlassen, auf dass sie weiter wachsen und gedeihen kann.</strong></p>
-              <p>Über ein Jahr lang haben wir als Team aus 10 Organisationen daran gearbeitet, die Bits&Bäume 2018 aus der Taufe zu heben – das war sehr bereichernd für uns, aber auch viel Arbeit, gerade weil so unterschiedliche Organisationen an einem Tisch saßen. Wir bekommen viele freudige Hoffnungsbekundungen auf eine Wiederholung 2019, was uns sehr freut, allerdings können wir leider diese gemeinsame Arbeit nicht langfristig in unseren Arbeitsalltag integrieren. Aber die Bits & Bäume war auch eine <strong>Konferenz von uns allen für uns alle!</strong> Nehmt den Faden also doch einfach auf, gründet einen Stammtisch in eurer Stadt, organisiert eine kleinere oder größere Veranstaltung, einen Diskussionsabend, erklärt auf Panels, warum und wie Digitalisierung und Nachhaltigkeit zusammengehören, lasst uns weiterhin unter #bitsundbäume Ideen und Fortschritte austauschen, denkt euch neue Vernetzungsformate aus.</p>              
+              <p>Über ein Jahr lang haben wir als Team aus 10 Organisationen daran gearbeitet, die Bits&Bäume 2018 aus der Taufe zu heben – das war sehr bereichernd für uns, aber auch viel Arbeit, gerade weil so unterschiedliche Organisationen an einem Tisch saßen. Wir bekommen viele freudige Hoffnungsbekundungen auf eine Wiederholung 2019, was uns sehr freut, allerdings können wir leider diese gemeinsame Arbeit nicht langfristig in unseren Arbeitsalltag integrieren. Aber die Bits & Bäume war auch eine <strong>Konferenz von uns allen für uns alle!</strong> Nehmt den Faden also doch einfach auf, gründet einen Stammtisch in eurer Stadt, organisiert eine kleinere oder größere Veranstaltung, einen Diskussionsabend, erklärt auf Panels, warum und wie Digitalisierung und Nachhaltigkeit zusammengehören, lasst uns weiterhin unter <a href="https://twitter.com/hashtag/bitsundbaeume">#bitsundbäume</a> Ideen und Fortschritte austauschen, denkt euch neue Vernetzungsformate aus.</p>              
             </section>
             <section>
               <h2>Nächste B&B-Veranstaltung</h2>
@@ -49,7 +94,7 @@ Not yet available in English!
               </ul>
           </section>
           <section>
-<h2>Folgendes legen wir Euch zusätzlich ans Herz:</h2>
+<h2>Folgendes legen wir Euch zusätzlich ans Herz</h2>
 <ul>
     <li><strong>Beteiligung fördern</strong>: Falls ihr eine Konferenz inkl. Vorträgen und Workshops veranstaltet, startet bitte einen Aufruf zur Beteiligung am Programm und verbreitet ihn so weit ihr könnt – denn nur so könnt ihr auch Ideen und Menschen einbeziehen, die ihr noch nicht kennt und es können neue Netzwerke entstehen. </li>
     <li>V<strong>ernetzung ermöglichen</strong>: Sorgt auch vor Ort explizit für Zeit und Raum zum Austausch und für Vernetzung. Verwendet gern und viel den Hashtag <a href="https://twitter.com/hashtag/bitsundbaeume">#bitsundbäume</a>, z.B. auf Mastodon, Twitter oder Diaspora; wir greifen das via @bitsundbaeume gerne auf. </li>
