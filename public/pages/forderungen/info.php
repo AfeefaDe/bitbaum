00dfeed3811e37d3ae3f8164b448f694713414ed
foobar0112 <foobar2@posteo.de>
2019-05-23 11:29:39 +0200

    merged german and english demands

--- public/pages/forderungen/info.php
+++ public/pages/forderungen/info.php
@@ -14,48 +14,189 @@
         margin-top: 0;
     }
 </style>
+
+
+<?php
+//TODO: style status response
+if (isset($_GET['status']) && in_array($_GET['status'], [0, 1, 2, 3, 4, 5])) {
+    echo "<div class='status'>";
+    switch ($_GET['status']) {
+        case 0:
+            echo "some error occured";
+            break;
+        case 1:
+            echo "support verified";
+            break;
+        case 2:
+            echo "support published";
+            break;
+        case 3:
+            echo "code invalid, already verified";
+            break;
+        case 4:
+            echo "code invalid, already published";
+            break;
+        case 5:
+            echo "code invalid";
+            break;
+    }
+}
+echo "</div>";
+?>
+
 <?php switch ($lang) {
     case "en":
         ?>
-        <p>This page is only available in <a href="/forderungen/de">German</a>. You can find a translation of <strong>our
-                demands</strong> near the end of this <a href="https://www.degrowth.info/en/2018/12/a-conference-for-digitalization-and-sustainability-reflections-on-bits-baume/
-" target="_blank">article</a>. </p>
-        <p><em>German content:</em> <br></p>
-    <?php
-default:
+        <article>
+            <h1>Demands</h1>
+            <div class="row">
+                <section>
+                    <p>The jointly organized conference “Bits & Bäume” allowed us to initiate a broad and much needed
+                        discussion on technology design which serves the common good and peace, takes data protection
+                        seriously and promotes social and ecological goals in equal measure. This event, which was a
+                        collaborative effort by ten organizations from the fields of digital rights, environmentalism
+                        and development policy, hosted over 1,700 visitors, countless sessions, ideas and outstanding
+                        use cases. This shows: civil society and a critical scientific community have the skills and the
+                        strength to help shape digitization in the long term.</p>
+                    <p>We cannot merely leave digitization to business and politics. From now on, we will be even more
+                        present and united in both public discourse and practical implementation. Together we make the
+                        following demands:</p>
+                </section>
+                <section>
+                    <a href="/downloads/BuB-Forderungen.jpg"><img style="max-width: 100%" class="portrait"
+                                                                  src="/img/BuB-Forderungen-small.jpg"></a>
+                    <p class="smaller">CC BY Santiago Engelhardt</p>
+                    <p class="smaller">Back row, left to right: Rainer Rehak (FIfF), Thomas Korbun (IÖW), Rolf Buschmann
+                        (BUND), Sven Hilbig (Brot für die Welt), Maria Bossmann (DNR), Constanze Kurz (CCC), Nadine
+                        Evers (OKF DE)</p>
+                    <p class="smaller">Front row, left to right: Christoph Bals (Germanwatch), Nina Treu (Konzeptwerk
+                        Neue Ökonomie), Tilman Santarius (TU Berlin)</p>
+                </section>
+            </div>
+
+            <section>
+                <h2>Social-ecological objectives in the design of digitization</h2>
+                <div class="row row--forderung">
+                    <span>1.</span>
+                    <p>Digitization must serve the common good. It shall not pursue only the promotion of an economic
+                        growth agenda, but must aim to promote social, environmental and development policies as well as
+                        peace objectives. Digitization must contribute to a sustainable transformation of energy,
+                        transport, agriculture and resource policy. Moreover, digitization shall foster human rights,
+                        climate protection goals as well as the end of hunger and poverty. A sustainable digitalization
+                        in our definition relies on meaningful, decent work, social justice and sufficient
+                        lifestyles.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>Democracy</h2>
+                <div class="row row--forderung">
+                    <span>2.</span>
+                    <p>Digitization must be made more democratic in itself and at the same time support democratic
+                        processes instead of counteracting them. To this end, it must be consistently geared to
+                        promoting emancipatory potential, decentralized participation, open innovation and civil society
+                        commitment.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>Data protection and control of monopolies</h2>
+                <div class="row row--forderung">
+                    <span>3.</span>
+                    <p>
+                        Data protection, freedom from manipulation and informational self-determination shall be
+                        promoted both nationally and globally as essential prerequisites for free, democratic, peaceful
+                        and sovereign societies.</p>
+                </div>
+                <div class="row row--forderung">
+                    <span>4.</span>
+                    <p>We need to create basic conditions for controlling digital monopolies in order to enable a
+                        self-determined digital economy in the North and the global South. Existing monopolies of
+                        operators of commercial platforms must be broken, for example by imposing a mandatory predefined
+                        interface for exchange between social media services.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>Education</h2>
+                <div class="row row--forderung">
+                    <span>5.</span>
+                    <p>Political regulation must begin to see knowledge and education about technology and its
+                        consequences as part of the public good; this has to become an elementary component of public
+                        knowledge. A critical and emancipatory handling of digital technology should be part of digital
+                        education, including the competent handling of false information and hate speech in digital
+                        media.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>Aspects of development and trade policy</h2>
+                <div class="row row--forderung">
+                    <span>6.</span>
+                    <p>Countries of the global South must have the opportunity to develop their own digitization
+                        according to local and national needs. The costs and benefits of digitization shall be shared
+                        equally between all societies. Negative aspects, such as inhumane working conditions,
+                        environmental pollution, damage to health and electronic waste must not be unilaterally passed
+                        on to the global South.</p>
+                </div>
+                <div class="row row--forderung">
+                    <span>7.</span>
+                    <p>Bilateral and multilateral trade agreements must not contain any prohibitions or restrictions
+                        regarding taxation, open source disclosure or localization.</p>
+                </div>
+                <div class="row row--forderung">
+                    <span>8.</span>
+                    <p>The technology sector must be obliged to respect human rights and ecological due diligence in the
+                        mining and production countries when faced with issues of resource conservation and
+                        sustainability.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>IT security</h2>
+                <div class="row row--forderung">
+                    <span>9.</span>
+                    <p>Deficient software has negative consequences for its users, the security of their data and the
+                        digital infrastructure as a whole. Software liability is needed so that software developers bear
+                        the responsibility for the risks that arise (e.g. security gaps) instead of subjecting the
+                        quality of their software to profit. IT security is the foundation of a sustainable digital
+                        society.</p>
+                </div>
+            </section>
+
+            <section>
+                <h2>Longevity of software and hardware</h2>
+                <div class="row row--forderung">
+                    <span>10.</span>
+                    <p>Software must be adaptable to individual use, repairable and fit for long-term use, as is the
+                        case with open source software. For example, manufacturers must provide security updates
+                        throughout the lifetime of hardware-devices and make a variant of the source code open source at
+                        the end of support instead of installing “software locks”.</p>
+                </div>
+                <div class="row row--forderung">
+                    <span>11.</span>
+                    <p>Electronic devices must be repairable and recyclable – we have to eradicate planned obsolescence
+                        or built-in obsolescence of electronic devices. To this end, warranty periods must be massively
+                        extended; manufacturers must offer spare parts, repair tools and know-how for everyone and keep
+                        it permanently available. This must go hand in hand with greater financial support for open
+                        workshops or repair cafés as well as for Research & Development projects geared to the common
+                        good. Public funding must be granted to open source products exclusively.</p>
+                </div>
+            </section>
+            <section>
+                This unofficial translation by Nicolas Guenot, Nina Treu and Nick originates <a
+                        href="https://www.degrowth.info/en/2018/12/a-conference-for-digitalization-and-sustainability-reflections-on-bits-baume/">here</a>.
+            </section>
+
+        </article>
+        <?php
+        break;
+    default:
         ?>
         <article>
             <h1>Forderungen</h1>
 
-            <?php
-
-        if (isset($_GET['status']) && in_array($_GET['status'], [0, 1, 2, 3, 4, 5])) {
-            echo "<div class='status'>";
-            switch ($_GET['status']) {
-                case 0:
-                    echo "some error occured";
-                    break;
-                case 1:
-                    echo "support verified";
-                    break;
-                case 2:
-                    echo "support published";
-                    break;
-                case 3:
-                    echo "code invalid, already verified";
-                    break;
-                case 4:
-                    echo "code invalid, already published";
-                    break;
-                case 5:
-                    echo "code invalid";
-                    break;
-            }
-        }
-        echo "</div>";
-        ?>
-
-              <div class="row">
+            <div class="row">
                 <section>
                     <p>Wir, zehn Organisationen aus der Netz-, Umwelt- und Entwicklungspolitik, haben mit der Konferenz
                         „Bits & Bäume“ eine gemeinsame Grundlage dafür geschaffen, wie die Digitalisierung so gestaltet
@@ -66,12 +207,14 @@ default:
                         zeigen: Die Zivilgesellschaft und eine kritische Wissenschaft haben gemeinsam die
                         Wissensgrundlage, die Kompetenzen, die Weitsicht, den Willen und auch die Kraft, eine
                         nachhaltige Digitalisierung mitzugestalten, wenn sie sich zusammenschließen.</p>
-                    <p class="moreContent">Wir können die Digitalisierung nicht alleine der Wirtschaft und der Politik überlassen. Deshalb
+                    <p class="moreContent">Wir können die Digitalisierung nicht alleine der Wirtschaft und der Politik
+                        überlassen. Deshalb
                         werden wir uns zukünftig noch stärker und gemeinsam in die gesellschaftliche Diskussion und die
                         praktische Umsetzung einbringen.</p>
                     <span class="readmore smaller">Mehr lesen</span>
                     <p>
-                      <strong>Als Bündnis von Organisationen mit zehn verschiedenen Perspektiven auf die notwendigen Veränderungen stellen wir gemeinsam diese Forderungen:</strong>
+                        <strong>Als Bündnis von Organisationen mit zehn verschiedenen Perspektiven auf die notwendigen
+                            Veränderungen stellen wir gemeinsam diese Forderungen:</strong>
                     </p>
                 </section>
                 <section>
@@ -79,10 +222,12 @@ default:
                                                                   src="/img/BuB-Forderungen-small.jpg"></a>
                     <p class="readmore smaller">Bildinfos anzeigen</p>
                     <p class="smaller moreContent">CC BY Santiago Engelhardt</p>
-                    <p class="smaller moreContent">Hintere Reihe von links: Rainer Rehak (FIfF), Thomas Korbun (IÖW), Rolf Buschmann
+                    <p class="smaller moreContent">Hintere Reihe von links: Rainer Rehak (FIfF), Thomas Korbun (IÖW),
+                        Rolf Buschmann
                         (BUND), Sven Hilbig (Brot für die Welt), Maria Bossmann (DNR), Constanze Kurz (CCC), Nadine
                         Evers (OKF DE)</p>
-                    <p class="smaller moreContent">Vordere Reihe von links: Christoph Bals (Germanwatch), Nina Treu (Konzeptwerk
+                    <p class="smaller moreContent">Vordere Reihe von links: Christoph Bals (Germanwatch), Nina Treu
+                        (Konzeptwerk
                         Neue Ökonomie), Tilman Santarius (TU Berlin)</p>
                 </section>
             </div>
@@ -201,11 +346,11 @@ default:
             </section>
 
             <section id="unterzeichnen">
-              <h2>Forderungen unterzeichnen</h2>
+                <h2>Forderungen unterzeichnen</h2>
                 <?php
-require_once 'components/support_form.php';
-        ?>
+                require_once 'components/support_form.php';
+                ?>
             </section>
 
         </article>
-    <?php }?>
+    <?php } ?>
