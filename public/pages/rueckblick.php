5230056606b93759936e0d8522a855abc8a30a23
Felix <fische@courages.net>
2019-06-23 22:08:41 +0200

    optimize and translate rueckblick

--- public/pages/rueckblick.php
+++ public/pages/rueckblick.php
@@ -18,6 +18,32 @@ article .row--forderung > p {
 
   <article>
     <h1>That was 2018</h1>
+
+    <div class="row">
+
+            <section>
+              <h2>In Kürze</h2>
+              <p><strong>Bits & Bäume Konferenz</strong><br>17./18. November 2018 | Campus TU Berlin</p>
+              <a href="https://media.ccc.de/c/bub2018" class="button" target="_blank" style="text-align: center">Audio- und Videoaufzeichnungen</a>
+            </section>
+
+            <section>
+              <p><a target="_blank" href="/fotogalerie">Fotogalerie</a></p>
+              <p><a href="/info/<?php echo $lang; ?>">Ziele der Konferenz</a></p>
+              <p>
+                <a href="https://fahrplan.bits-und-baeume.org" target="_blank">Online-Programm</a> |
+                <a target="_blank" href="/downloads/bits-und-baeume-2018-programmheft.pdf">PDF-Programm</a>
+              </p>
+              <p><a href="/infrastruktur/<?php echo $lang; ?>">Nachhaltige Konferenzorganisation</a></p>
+              <p><a href="/presse/<?php echo $lang; ?>">Presseinformationen</a></p>
+            </section>
+          </div>
+
+          <section>
+            <h2>Programm</h2>
+            <p>Unser zweitägiges Programm umfasste mehr als 120 international besetzte Panels, Talks, Workshops, Forum, philosophischen Salon, Sporangium, Konzert und Apéro. Zahlreiche Organisationen, Personen und internationale Referierende haben das Programm durch ihre Einreichungen mitgestaltet und ihre Ansätze für eine nachhaltige Digitalisierung mit den Teilnehmenden geteilt. Gemeinsam haben wir an Lösungsideen für eine nachhaltige Zukunft getüftelt und unterstützen einander auch weiterhin dabei, die Welt zu verändern! <br><br>Das Programm unserer fünf Bühnen wurde vom Video Operation Center des CCC aufgezeichnet.</p>
+            <p><a href="/programm/<?php echo $lang; ?>">Programmbeschreibung</a></p>
+          </section>
   </article>
       <?php
 break;
@@ -30,13 +56,12 @@ break;
           <div class="row">
 
             <section>
-              <p>...</p>
-            </section>
-
-            <section>
               <h2>In Kürze</h2>
               <p><strong>Bits & Bäume Konferenz</strong><br>17./18. November 2018 | Campus TU Berlin</p>
               <a href="https://media.ccc.de/c/bub2018" class="button" target="_blank" style="text-align: center">Audio- und Videoaufzeichnungen</a>
+            </section>
+
+            <section>
               <p><a target="_blank" href="/fotogalerie">Fotogalerie</a></p>
               <p><a href="/info/<?php echo $lang; ?>">Ziele der Konferenz</a></p>
               <p>
