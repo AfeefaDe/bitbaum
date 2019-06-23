3e322cecd25d2cab58dcef62f7adae10016e6604
Felix <fische@courages.net>
2019-06-23 03:19:46 +0200

    translate supporters list

--- public/pages/forderungen/unterzeichner.php
+++ public/pages/forderungen/unterzeichner.php
@@ -1,7 +1,9 @@
 <?php switch ($lang) {
     case "en":
         ?>
-        <h1>Supporter</h1>
+        <h1>Full list of supporters</h1>
+        <p><strong>The following people support our <a href="/forderungen/<?php echo $lang; ?>">demands</a> and signed them.</strong></p>
+        <hr>
         <?php
 break;
     default:
@@ -9,19 +11,15 @@ break;
         <h1>Unterstützer*innenliste</h1>
         <p><strong>Die folgenden Personen unterstützen unserer <a href="/forderungen/<?php echo $lang; ?>">Forderungen</a> und haben diese unterzeichnet.</strong></p>
         <hr>
-        <div class="support-records">
+
+    <?php }?>
+
+    <div class="support-records">
         <?php
 $supports = SupportController::getSupports(5);
 
-        foreach ($supports as $key => $support) {
-            require 'components/support_record.php';
-        }
-        ?>
+foreach ($supports as $key => $support) {
+    require 'components/support_record.php';
+}
+?>
 </div>
-        <section>
-            <?php
-// require_once 'components/support_form.php';
-        ?>
-        </section>
-
-    <?php }?>
