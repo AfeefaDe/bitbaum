64e6d7dca4a14de552a67bf0d406e72b7a558c4b
Felix <fische@courages.net>
2019-06-23 21:53:50 +0200

    translate sidebar forderungen

--- public/components/sidebar_forderungen.php
+++ public/components/sidebar_forderungen.php
@@ -3,6 +3,33 @@
     case "en":
         ?>
 
+<section class="switch support">
+
+    <h2>
+      <span><?php echo SupportController::getTotalNumberOfSupports(); ?> Supporters</span>
+      <a href="/forderungen/unterzeichner/<?php echo $lang; ?>">
+        <span class="smaller">See full list</span>
+      </a>
+    </h2>
+  <p class="bold" style="margin-top: 0">already signed our demands.</p>
+
+<div class="records">
+<?php
+$supports = SupportController::getSupports(5);
+
+        foreach ($supports as $key => $support) {
+            require 'components/support_record.php';
+        }
+        ?>
+
+</div>
+<?php
+$url = (strpos($page, 'forderungen/info') !== false) ? '' : '/forderungen/' . $lang;
+        ?>
+<a href="<?php echo $url; ?>#unterzeichnen" class="button important" style="text-align: center"><span class="icon sign">Sign demands</span></a>
+<p class="smaller">Die Unterschriften geben den Forderungen mehr Gewicht, denn alle Beteiligten verweisen regelmäßig auf sie, bei Hintergrundgesprächen über öffentliche Auftritte bis hin zu Gesprächen mit Entscheidungsträger*innen aus allen möglichen Bereichen.</p>
+
+</section>
 
         <?php
 break;
