49eebb015079ff39589ae7941142249064f7a738
yuelika <32544557+yuelika@users.noreply.github.com>
2019-07-03 20:03:48 +0200

    delete phone number

--- public/components/contact.php
+++ public/components/contact.php
@@ -5,7 +5,7 @@
         ?>
             <p><strong>Contact</strong></p>
             <p>
-              <span class="mail_replace_info"></span> | <span>01753540452</span>
+              <span class="mail_replace_info"></span> 
             </p>
             <p>Bits&Bäume core team:<br>Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick</p>
             <p>Bits&Bäume office:<br>Ekaterina Sergienko, Eva Laderick, Katja George</p>
@@ -15,7 +15,7 @@ break;
         ?>
             <p><strong>Kontakt</strong></p>
             <p>
-              <span class="mail_replace_info"></span> | <span>01753540452</span>
+              <span class="mail_replace_info"></span>
             </p>
             <div class="row">
               <p>Kernteam der Bits&Bäume:<br>Anja Höfner, Constanze Kurz, Hendrik Zimmermann, Juliane Krüger, Leon Kaiser, Maria Bossmann, Nicolas Guenot, Sven Hilbig, Teresa Hoffmann, Tilman Santarius, Rainer Rehak, Rolf Buschmann, Vivian Frick</p>
