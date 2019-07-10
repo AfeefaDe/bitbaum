3063336c53f97a7bd5ff09274ead216fcf59b3c8
yuelika <32544557+yuelika@users.noreply.github.com>
2019-07-10 20:53:26 +0200

    update news en

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -9,15 +9,18 @@
 <section class="switch news active">
 
   <article>
-     <h3 class="bold">Stammtisch <em>Bits&Bäume Dresden</em>: July, 3rd, 6pm</h3>
-      <p>After two successfull <a href="https://dresden.bits-und-baeume.org" target="blank">Bits&Bäume Dresden </a>events in June, on June 20, a first regular's round met. Meeting point for the second round: <a href="http://m.osmtools.de/0qMuk32fa9H0qNI232fiU4" target="_blank">TU Dresden, Alte Mensa (Ostseite)</a>
+     <h3 class="bold">[Dresden] <em>Bits&Bäume</em> regular's round</h3>
+      <p>After two successfull Bits&Bäume events in Dresden, a regular's round was founded on the 20th of June at TU Dresden. More information <a href="https://dresden.bits-und-baeume.org" target="blank">here</a>
 </p>  
-     <br>
-    <h3 class="bold">Buchvorstellung <em>Was Bits&Bäume verbindet</em>: July, 3rd, 7pm</h3>
-    <p>Our <a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">Bits&Bäume publication </a>will be launched: 7-8:30pm at the Coworking Space EINS TU Berlin, registration <a href="https://www.ioew.de/was-bits-und-baeume-verbindet" target="_blank">here</a></p>
-    
+     <br />
+      <h3 class="bold">[Berlin] <em>Bits&Bäume</em> regular's round</h3>
+    <p>In Berlin the AStA of TU Berlin also founded a regular's round. More information <a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">here</a> 
+</p>  
+      <br />
+    <h3 class="bold">Publication <em>Was Bits&Bäume verbindet</em></h3>
+    <p>Our <a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">conference book</a> has been published pushing further the topics of the conference. Thanks to all crowdfunders</p>.
+      
     </article>
-
 </section>
 
 <section class="switch twitter">
@@ -46,18 +49,17 @@ break;
 <section class="switch news active">
 
   <article>
-      <h3 class="bold">Stammtisch <em>Bits&Bäume Dresden</em></h3>
+      <h3 class="bold">[Dresden] Stammtisch <em>Bits&Bäume</em></h3>
     <p>Nach zwei erfolgreichen Bits&Bäume-Veranstaltungen in Dresden hat sich am 20. Juni an der TU Dresden ein B&B-Stammtisch gegründet. Mehr Infos auf <a href="https://dresden.bits-und-baeume.org" target="blank">Bits&Bäume Dresden</a>. 
 </p>  
-     <br>
-      <h3 class="bold">Stammtisch <em>Bits&Bäume Berlin</em></h3>
+   <br />
+        <h3 class="bold">[Berlin] Stammtisch <em>Bits&Bäume</em></h3>
     <p>Auch in Berlin trifft sich ein B&B-Stammtisch, organisiert vom AStA der TU Berlin. Mehr Infos auf <a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">hier</a>. 
 </p>  
-      <br>
+      <br />
     <h3 class="bold"><em>Was Bits&Bäume verbindet</em> erschienen</h3>
-    <p>Das<a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">Buch zur Konferenz</a>ist erschienen. Dank an die successfully fundende Crowd!</p>
-
-    </article>
+    <p>Das <a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">Buch zur Konferenz</a> ist da. Dank an die successfully fundende Crowd!</p>
+ </article>
 
 </section>
 
@@ -95,7 +97,7 @@ function showTwitter () {
 
 function showNews () {
   $newsBtn.addClass('active');
-  $news.addClass('active');
+  $news.addClass('active'); 
   $twitterBtn.removeClass('active');
   $twitter.removeClass('active');
 }
