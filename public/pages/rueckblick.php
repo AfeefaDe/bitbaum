f104ee1a3124bf33eb76306339ba7a8ce1425da9
laryllian <748785+laryllian@users.noreply.github.com>
2019-07-10 21:53:42 +0200

    ul

--- public/pages/rueckblick.php
+++ public/pages/rueckblick.php
@@ -64,16 +64,16 @@ break;
               <a href="https://media.ccc.de/c/bub2018" class="button" target="_blank" style="text-align: center">Audio- und Videoaufzeichnungen</a>
             </section>
 
-            <section>
-              <p><a target="_blank" href="/fotogalerie">Fotogalerie</a></p>
-              <p><a href="/info/<?php echo $lang; ?>">Ziele der Konferenz</a></p>
-              <p>
+            <section><ul>
+              <li><a target="_blank" href="/fotogalerie">Fotogalerie</a></li>
+              <li><a href="/info/<?php echo $lang; ?>">Ziele der Konferenz</a></li>
+              <li>
                 <a href="https://fahrplan.bits-und-baeume.org" target="_blank">Online-Programm</a> |
                 <a target="_blank" href="/downloads/bits-und-baeume-2018-programmheft.pdf">PDF-Programm</a>
-              </p>
-              <p><a href="/infrastruktur/<?php echo $lang; ?>">Nachhaltige Konferenzorganisation</a></p>
-              <p><a href="/presse/<?php echo $lang; ?>">Presseinformationen</a></p>
-            </section>
+              </li>
+              <li><a href="/infrastruktur/<?php echo $lang; ?>">Nachhaltige Konferenzorganisation</a></li>
+              <li><a href="/presse/<?php echo $lang; ?>">Presseinformationen</a></li>
+            </ul></section>
           </div>
 
           <section>
