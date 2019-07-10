0af414d92d258ceb1335b7d7c16cfec31468884d
yuelika <32544557+yuelika@users.noreply.github.com>
2019-07-10 22:38:33 +0200

    add links en

--- public/pages/waechst.php
+++ public/pages/waechst.php
@@ -26,6 +26,14 @@ article .row--forderung > p {
             <section>
               <h2>B&B Communities</h2>
 <ul>
+   <h2>B&B materials</h2>
+ <ul>
+  <li>🗋 B&B logo<a target="_blank" href="/downloads/bits-und-baeume-logo.pdf"> solo</a> | <a target="_blank" href="/downloads/bits-und-baeume-logo-schrift.pdf>with German conference subtitle </a>[pdf]</li>
+  <li>🗋 <a href="/downloads/BUB_Sticker_zeitlos.pdf" target="blank">B&B sticker</a></li>
+  <li>🗋 B&B-Forderungen als Broschüre <a href="/downloads/BUB_Forderungen_A5_Einzelseiten.pdf" target="blank">single pages </a>| <a href="/downloads/BUB_Forderungen_A5_Doppelseiten_Bookletreihenfolge.pdf" target="blank">double pages for brochure printing</a>[pdf]</li>
+  <li>🗋 <a target="_blank" href="/downloads/B&B_Plakat_DIN-A1_CC_181108.pdf">B&B poster</a> [pdf]</li>
+  <li><a href="https://bits-und-baeume.org/infrastruktur/de" target="blank">how to organize sustainably</a></li> 
+</ul> 
    <li><a href="https://dresden.bits-und-baeume.org" target="blank">Bits & Bäume Dresden</a>
     <br>initiated by the Faculty of Computer Science, TU Dresden: Bits&Bäume events on May 23rd and June 20th, Bits&Bäume regular's round </li>
    <li><a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">Bits & Bäume Berlin</a>
@@ -87,11 +95,11 @@ break;
 </ul>
                 <h2>B&B-Material</h2>
  <ul>
-  <li>🗋 B&B-Logo<a target="_blank" href="/downloads/bits-und-baeume-logo.pdf"></a> solo | <a target="_blank" href="/downloads/bits-und-baeume-logo-schrift.pdfmit Konferenz-Untertitel </a>[pdf]</li>
-  <li><a href="/downloads/BUB_Sticker_zeitlos.pdf" target="blank">B&B-Sticker</a></li>
-    <li>B&B-Forderungen als Broschüre <a href="/downloads/BUB_Forderungen_A5_Einzelseiten.pdf" target="blank">Einzelseiten für die Druckerei </a>| <a href="/downloads/BUB_Forderungen_A5_Doppelseiten_Bookletreihenfolge.pdf" target="blank">Doppelseiten zum Selberdrucken </a>[pdf]</li>
+  <li>🗋 B&B-Logo<a target="_blank" href="/downloads/bits-und-baeume-logo.pdf"> solo</a> | <a target="_blank" href="/downloads/bits-und-baeume-logo-schrift.pdf>mit Konferenz-Untertitel </a>[pdf]</li>
+  <li>🗋 <a href="/downloads/BUB_Sticker_zeitlos.pdf" target="blank">B&B-Sticker</a></li>
+  <li>🗋 B&B-Forderungen als Broschüre <a href="/downloads/BUB_Forderungen_A5_Einzelseiten.pdf" target="blank">Einzelseiten für die Druckerei </a>| <a href="/downloads/BUB_Forderungen_A5_Doppelseiten_Bookletreihenfolge.pdf" target="blank">Doppelseiten zum Selberdrucken </a>[pdf]</li>
   <li>🗋 <a target="_blank" href="/downloads/B&B_Plakat_DIN-A1_CC_181108.pdf">B&B-Plakat</a> [pdf]</li>
-   <li><a href="https://bits-und-baeume.org/infrastruktur/de" target="blank">Tipps des B&B-Teams: nachhaltig organisieren</a></li> 
+  <li><a href="https://bits-und-baeume.org/infrastruktur/de" target="blank">Tipps des B&B-Teams: nachhaltig organisieren</a></li> 
 </ul> 
             </section>
           </div>
