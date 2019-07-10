d4ac64a87fc072f6380be6c6807ce03bf485d2cf
yuelika <32544557+yuelika@users.noreply.github.com>
2019-07-10 20:19:56 +0200

    update Aktuelles de

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -28,7 +28,7 @@
 <div class="control">
     <div class="news active">
         <div class="icon"></div>
-        <p>News</p>
+        <p>news</p>
     </div>
     <div class="twitter">
         <div class="icon"></div>
@@ -46,12 +46,16 @@ break;
 <section class="switch news active">
 
   <article>
-      <h3 class="bold">Stammtisch <em>Bits&Bäume Dresden</em>: 3. Juli, 18 Uhr</h3>
-    <p>Nach zwei erfolgreichen Bits&Bäume-Veranstaltungen in Dresden hat sich am 20. Juni ein Stammtisch der <a href="https://dresden.bits-und-baeume.org">Bits&Bäume Dresden</a> gegründet, der nun in die zweite Runde geht. Treffpunkt: <a href="http://m.osmtools.de/0qMuk32fa9H0qNI232fiU4" target="_blank">TU Dresden, Alte Mensa (Ostseite)</a>
+      <h3 class="bold">Stammtisch <em>Bits&Bäume Dresden</em></h3>
+    <p>Nach zwei erfolgreichen Bits&Bäume-Veranstaltungen in Dresden hat sich am 20. Juni an der TU Dresden ein B&B-Stammtisch gegründet. Mehr Infos auf <a href="https://dresden.bits-und-baeume.org" target="blank">Bits&Bäume Dresden</a>. 
 </p>  
      <br>
-    <h3 class="bold">Buchvorstellung <em>Was Bits&Bäume verbindet</em>: 3. Juli, 19 Uhr</h3>
-    <p>Wir stellen unser successfully crowdfunded <a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">Buch </a>vor. Du bist herzlich eingeladen, dabei zu sein. Von 19:00 – 20:30h im Coworking Space EINS TU Berlin, Anmeldung <a href="https://www.ioew.de/was-bits-und-baeume-verbindet" target="_blank">hier</a></p>
+      <h3 class="bold">Stammtisch <em>Bits&Bäume Berlin</em></h3>
+    <p>Auch in Berlin trifft sich ein B&B-Stammtisch, organisiert vom AStA der TU Berlin. Mehr Infos auf <a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">hier</a>. 
+</p>  
+      <br>
+    <h3 class="bold"><em>Was Bits&Bäume verbindet</em> erschienen</h3>
+    <p>Das<a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="blank">Buch zur Konferenz</a>ist erschienen. Dank an die successfully fundende Crowd!</p>
 
     </article>
 
@@ -65,7 +69,7 @@ break;
 <div class="control">
     <div class="news active">
         <div class="icon"></div>
-        <p>News</p>
+        <p>Aktuelles</p>
     </div>
     <div class="twitter">
         <div class="icon"></div>
