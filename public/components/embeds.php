d8b9ee7135b1f7b18b9844f1a42ab0946f67152d
yuelika <32544557+yuelika@users.noreply.github.com>
2019-09-19 23:42:32 +0200

    add global climate strike banner

--- public/components/embeds.php
+++ public/components/embeds.php
@@ -1,3 +1,4 @@
 <!-- Stylesheets -->
 <link rel="stylesheet" href="/styles/styles.min.css">
 <script src="/js/jquery.min.js"></script>
+<script src="https://assets.digitalclimatestrike.net/widget.js" async></script>
