105ce24326f890939aeb3b3ea9cd6914e352b964
yuelika <32544557+yuelika@users.noreply.github.com>
2019-09-22 15:44:25 +0200

    Remove climate strike banner

--- public/components/embeds.php
+++ public/components/embeds.php
@@ -1,13 +1,3 @@
 <!-- Stylesheets -->
 <link rel="stylesheet" href="/styles/styles.min.css">
 <script src="/js/jquery.min.js"></script>
-
-<!-- DIGITAL CLIMATE STRIKE SECTION -->
-<script type="text/javascript">
-  var DIGITAL_CLIMATE_STRIKE_OPTIONS = {
-    disableGoogleAnalytics: true,
-    language: de,
-    iframeHost: 'https://bits-und-baeume.org',
-  };
-</script>
-<script src="/js/widget.js"></script>
