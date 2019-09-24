32ed7aa97c1e716c7e297f6b9c26f69bf57d78b6
felix <felix@afeefa.de>
2019-09-24 12:38:29 +0200

    add final content to publish /forum page

--- public/pages/forum.php
+++ public/pages/forum.php
@@ -2,13 +2,80 @@
     ul {
         list-style-type: "➤ ";
     }
+
+    .event h2 {
+        margin-top: 0.5em;
+    }
+
+    .heading {
+        border-bottom: 0.5px solid gray;
+        font-weight: bold;
+        padding-bottom: 0.5em;
+        margin-top: 2em;
+    }
 </style>
 <?php switch ($lang) {
     case "en":
         ?>
 
         <article>
+            
+            <h1>Forum Bits & Bäume 2019</h1>
 
+            <div class="row">
+
+                <section>
+                    <p class="bold">Über die Gesprächsreihe</p>
+                    <p>Das Forum Bits & Bäume bietet über zwei Jahre einen offenen Diskussionsraum, um die nachhaltige Gestaltung der Digitalisierung stärker ins Zentrum zu rücken und wissenschaftliche und zivilgesellschaftliche Themen und Forderungen in den politischen Diskurs einzubringen. Die Veranstaltungen werden in dialogischen Formaten mit aktiven Workshop-Elementen durchgeführt, um konkrete Politikvorschläge und Gestaltungsoptionen zu erarbeiten und für die öffentliche und politische Diskussion aufzubereiten.</p>
+                </section>
+
+                <section>
+                    <p class="bold">Nächste Termine</p>
+                    <ul>
+                        <li>26.11.2019 in Berlin</li>
+                    </ul>
+                </section>
+            </div>
+
+            <section>
+                <p class="heading">Aktuelle Veranstaltung</p>
+
+                <div class="row event">
+                    <div>
+                        <h2>Ökologische Digitalsteuer</h2>
+                        <p class="bold">Welche politischen Instrumente machen die Digitalisierung zukunftsfähig?</p>
+                        <p>Digitalisierung und Klimaschutz – zwei heiße Eisen, über deren politische Ausgestaltung derzeit viel diskutiert wird. Im Gespräch ist dabei vor allem ein alter Bekannter: Steuern. Eine Digitalsteuer, damit Digitalkonzerne zur Finanzierung öffentlicher Aufgaben beitragen. Eine CO2-Steuer oder ein erweiterter Emissionshandel, um klimaschädliche Emissionen zu senken. Im zweiten Forum Bits & Bäume werden die beiden Herausforderungen Digitalisierung und Klima zusammengedacht. Wie können Steuern und andere Instrumente in der Digitalen Ökonomie ökologische Lenkungswirkung entfalten? Reichen Konzepte wie die CO2-Steuer oder bedarf es gesonderter Instrumente? Braucht es neben nationalen Instrumenten einen CO2-Zoll für die digitale Welt oder eine Steuer auf Datenverkehr?</p>
+                    </div>
+                    <div>
+                        <p><span class="bold">26. November 2019</span> / 16-19 Uhr</p>
+                        <p>im Einstein Center Digital Future<br>Wilhelmstraße 67, 10117 Berlin</p>
+                        <p><a class="button" href="https://www.nachhaltige-digitalisierung.de/bits-baeume/forum-bits-baeume.html" target="_blank">Zur Anmeldung</a></p>
+                    </div>
+                </div>
+
+            </section>
+
+            <section>
+                <p class="heading">Vergangene Veranstaltungen</p>
+
+                <div class="row event">
+                    <div>
+                        <h2>Künstliche Intelligenz für nachhaltige Entwicklung?</h2>
+                        <p>Künstliche Intelligenz (KI) ist derzeit in aller Munde und wirft viele Fragen zu wirtschaftspolitischen, ethischen, sozialen und ökologischen Erwartungen und Umsetzungspotenzialen auf. Bei dem ersten Forum Bits & Bäume diskutierten wir grundsätzliche Aspekte: Was kann KI zur Transformation der Gesellschaft in Richtung Nachhaltigkeit beitragen? Welche Anwendungsbeispiele gibt es, die eine Nutzung von KI für ökologische oder soziale Zielsetzungen ermöglichen? Was sind politische Gestaltungsoptionen, um sicherzustellen, dass KI-Anwendungen dem Gemeinwohl dienen?</p>
+                    </div>
+                    <div>
+                        <img style="max-width: 100%" class="portrait" src="/img/FBB1-fishbowl.jpg">
+                        <p><span class="bold">10. September 2019</span></p>
+                        <p>im Einstein Center Digital Future<br>ilhelmstraße 67, 10117 Berlin</p>
+                        <p class="info bold">Ein Diskussionspapier zur Veranstaltung wird derzeit noch um die Ergebnisse des ersten Forums angereichert und erscheint bald hier in aktualisierter Form.</p>
+                    </div>
+                </div>
+
+            </section>
+
+            <section>
+                <p class="smaller">___<br>Das Forum Bits & Bäume wird von der wissenschaftlichen Forschungsgruppe „Digitalisierung und sozial-ökologische Transformation“ von IÖW und TU Berlin veranstaltet in Kooperation mit dem Einstein Center Digital Future und weiteren wechselnden Partnern. Das Projekt wird durch das Bundesministerium für Bildung und Forschung (BMBF) im Förderschwerpunkt sozial-ökologische Forschung (SÖF) gefördert.</p>
+            </section>
         </article>
 
     <?php
@@ -29,47 +96,42 @@
                 <section>
                     <p class="bold">Nächste Termine</p>
                     <ul>
-                        <li>32.10.2019 in Berlin</li>
+                        <li>26.11.2019 in Berlin</li>
                     </ul>
                 </section>
             </div>
 
             <section>
-                <h2>Aktuelle Veranstaltung</h2>
+                <p class="heading">Aktuelle Veranstaltung</p>
 
                 <div class="row event">
                     <div>
-                        <h3>Nex big thing</h3>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                        <h2>Ökologische Digitalsteuer</h2>
+                        <p class="bold">Welche politischen Instrumente machen die Digitalisierung zukunftsfähig?</p>
+                        <p>Digitalisierung und Klimaschutz – zwei heiße Eisen, über deren politische Ausgestaltung derzeit viel diskutiert wird. Im Gespräch ist dabei vor allem ein alter Bekannter: Steuern. Eine Digitalsteuer, damit Digitalkonzerne zur Finanzierung öffentlicher Aufgaben beitragen. Eine CO2-Steuer oder ein erweiterter Emissionshandel, um klimaschädliche Emissionen zu senken. Im zweiten Forum Bits & Bäume werden die beiden Herausforderungen Digitalisierung und Klima zusammengedacht. Wie können Steuern und andere Instrumente in der Digitalen Ökonomie ökologische Lenkungswirkung entfalten? Reichen Konzepte wie die CO2-Steuer oder bedarf es gesonderter Instrumente? Braucht es neben nationalen Instrumenten einen CO2-Zoll für die digitale Welt oder eine Steuer auf Datenverkehr?</p>
                     </div>
                     <div>
-                        <p>
-                            <span class="bold">32. Oktober 2019</span><br>
-                            im Einstein Center Digital Future, Wilhelmstraße 67, 10117 Berlin
-                        </p>
-                        <p><a class="button" href="https://www.nachhaltige-digitalisierung.de/bits-baeume/forum-bits-baeume.html" target="_blank">Jetzt anmelden</a></p>
+                        <p><span class="bold">26. November 2019</span> / 16-19 Uhr</p>
+                        <p>im Einstein Center Digital Future<br>Wilhelmstraße 67, 10117 Berlin</p>
+                        <p><a class="button" href="https://www.nachhaltige-digitalisierung.de/bits-baeume/forum-bits-baeume.html" target="_blank">Zur Anmeldung</a></p>
                     </div>
                 </div>
 
             </section>
 
             <section>
-                <h2>Vergangene Veranstaltungen</h2>
+                <p class="heading">Vergangene Veranstaltungen</p>
 
                 <div class="row event">
                     <div>
-                        <h3>Künstliche Intelligenz für nachhaltige Entwicklung?</h3>
-                        <p>Künstliche Intelligenz (KI) ist derzeit in aller Munde und wirft viele Fragen zu wirtschaftspolitischen, ethischen, sozialen und ökologischen Erwartungen und Umsetzungspotenzialen auf. Wir diskutieren grundsätzliche Aspekte: Was kann KI zur Transformation der Gesellschaft in Richtung Nachhaltigkeit beitragen? Welche Anwendungsbeispiele gibt es, die eine Nutzung von KI für ökologische oder soziale Zielsetzungen ermöglichen? Was sind politische Gestaltungsoptionen, um sicherzustellen, dass KI-Anwendungen dem Gemeinwohl dienen?</p>
+                        <h2>Künstliche Intelligenz für nachhaltige Entwicklung?</h2>
+                        <p>Künstliche Intelligenz (KI) ist derzeit in aller Munde und wirft viele Fragen zu wirtschaftspolitischen, ethischen, sozialen und ökologischen Erwartungen und Umsetzungspotenzialen auf. Bei dem ersten Forum Bits & Bäume diskutierten wir grundsätzliche Aspekte: Was kann KI zur Transformation der Gesellschaft in Richtung Nachhaltigkeit beitragen? Welche Anwendungsbeispiele gibt es, die eine Nutzung von KI für ökologische oder soziale Zielsetzungen ermöglichen? Was sind politische Gestaltungsoptionen, um sicherzustellen, dass KI-Anwendungen dem Gemeinwohl dienen?</p>
                     </div>
                     <div>
-                        <p>
-                            <span class="bold">10. September 2019</span><br>
-                            im Einstein Center Digital Future, Wilhelmstraße 67, 10117 Berlin
-                        </p>
-                        <p class="info">
-                            <span class="bold">Veranstalter:</span><br>
-                            Institut für ökologische Wirtschaftsforschung (IÖW) und TU Berlin, Fachgebiet Sozial-ökologische Transformation in Kooperation mit dem Einstein Center Digital Future. Gefördert durch das Bundesministerium für Bildung und Forschung (BMBF) im Förderschwerpunkt Sozial-ökologische Forschung (SÖF)
-                        </p>
+                        <img style="max-width: 100%" class="portrait" src="/img/FBB1-fishbowl.jpg">
+                        <p><span class="bold">10. September 2019</span></p>
+                        <p>im Einstein Center Digital Future<br>ilhelmstraße 67, 10117 Berlin</p>
+                        <p class="info bold">Ein Diskussionspapier zur Veranstaltung wird derzeit noch um die Ergebnisse des ersten Forums angereichert und erscheint bald hier in aktualisierter Form.</p>
                     </div>
                 </div>
 
