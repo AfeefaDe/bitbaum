c904497a19d490dfb80456b450c07b160b4ff05f
felix <felix@afeefa.de>
2019-11-14 05:39:33 +0100

    add translations for /forum page

--- public/pages/forum.php
+++ public/pages/forum.php
@@ -19,62 +19,62 @@
         ?>
 
         <article>
-            
+
             <h1>Forum Bits & Bäume 2019</h1>
 
             <div class="row">
 
                 <section>
-                    <p class="bold">Über die Gesprächsreihe</p>
-                    <p>Das Forum Bits & Bäume bietet über zwei Jahre einen offenen Diskussionsraum, um die nachhaltige Gestaltung der Digitalisierung stärker ins Zentrum zu rücken und wissenschaftliche und zivilgesellschaftliche Themen und Forderungen in den politischen Diskurs einzubringen. Die Veranstaltungen werden in dialogischen Formaten mit aktiven Workshop-Elementen durchgeführt, um konkrete Politikvorschläge und Gestaltungsoptionen zu erarbeiten und für die öffentliche und politische Diskussion aufzubereiten.</p>
+                    <p class="bold">About the series of discussions / discussion session</p>
+                    <p>For more than two years, the Forum Bits & Bäume offers an open discussion space in order to focus more on the sustainable design of digitization and to introduce scientific and civil society issues and demands into the political discourse. The events will be conducted in dialogue formats with active workshop elements in order to develop concrete policy proposals and design options and prepare them for public and political debates.</p>
                 </section>
 
                 <section>
-                    <p class="bold">Nächste Termine</p>
+                    <p class="bold">Next events</p>
                     <ul>
-                        <li>27.01.2019 in Berlin</li>
+                        <li>January 27th, 2019 in Berlin</li>
                     </ul>
                 </section>
             </div>
 
             <section>
-                <p class="heading">Aktuelle Veranstaltung</p>
+                <p class="heading">Current event</p>
 
                 <div class="row event">
                     <div>
-                        <h2>Ökologische Digitalsteuer</h2>
-                        <p class="bold">Welche politischen Instrumente machen die Digitalisierung zukunftsfähig?</p>
-                        <p>Digitalisierung und Klimaschutz – zwei heiße Eisen, über deren politische Ausgestaltung derzeit viel diskutiert wird. Im Gespräch ist dabei vor allem ein alter Bekannter: Steuern. Eine Digitalsteuer, damit Digitalkonzerne zur Finanzierung öffentlicher Aufgaben beitragen. Eine CO2-Steuer oder ein erweiterter Emissionshandel, um klimaschädliche Emissionen zu senken. Im zweiten Forum Bits & Bäume werden die beiden Herausforderungen Digitalisierung und Klima zusammengedacht. Wie können Steuern und andere Instrumente in der Digitalen Ökonomie ökologische Lenkungswirkung entfalten? Reichen Konzepte wie die CO2-Steuer oder bedarf es gesonderter Instrumente? Braucht es neben nationalen Instrumenten einen CO2-Zoll für die digitale Welt oder eine Steuer auf Datenverkehr?</p>
+                        <h2>Ecological digital tax</h2>
+                        <p class="bold">Which political instruments make digitalization fit for the future?</p>
+                        <p>Digitalization and climate protection - two hot potatoes whose political design is currently the subject of much debate. The main subject of discussion is an old acquaintance: Taxes. A digital tax, so that digital corporations can contribute to the financing of public tasks. A CO2 tax or extended emissions trading to reduce climate-damaging emissions. In the second forum, Bits & Bäume, the two challenges of digitalization and climate will be considered together. How can taxes and other instruments in the digital economy develop an ecological steering effect? Are concepts such as the CO2 tax sufficient or do we need separate instruments? Do we need a CO2 tax for the digital world or a tax on data traffic in addition to national instruments?</p>
                     </div>
                     <div>
-                        <p><span class="bold">27. Januar 2020</span> / 16-19 Uhr</p>
-                        <p>im Einstein Center Digital Future<br>Wilhelmstraße 67, 10117 Berlin</p>
-                        <p><a class="button" href="https://www.nachhaltige-digitalisierung.de/bits-baeume/forum-bits-baeume.html" target="_blank">Zur Anmeldung</a></p>
+                        <p><span class="bold">January 27th, 2020</span> / 4-7 pm</p>
+                        <p>at the Einstein Center Digital Future<br>Wilhelmstraße 67, 10117 Berlin</p>
+                        <p><a class="button" href="https://www.nachhaltige-digitalisierung.de/bits-baeume/forum-bits-baeume.html" target="_blank">Register here</a></p>
                     </div>
                 </div>
 
             </section>
 
             <section>
-                <p class="heading">Vergangene Veranstaltungen</p>
+                <p class="heading">Past Events</p>
 
                 <div class="row event">
                     <div>
-                        <h2>Künstliche Intelligenz für nachhaltige Entwicklung?</h2>
-                        <p>Künstliche Intelligenz (KI) ist derzeit in aller Munde und wirft viele Fragen zu wirtschaftspolitischen, ethischen, sozialen und ökologischen Erwartungen und Umsetzungspotenzialen auf. Bei dem ersten Forum Bits & Bäume diskutierten wir grundsätzliche Aspekte: Was kann KI zur Transformation der Gesellschaft in Richtung Nachhaltigkeit beitragen? Welche Anwendungsbeispiele gibt es, die eine Nutzung von KI für ökologische oder soziale Zielsetzungen ermöglichen? Was sind politische Gestaltungsoptionen, um sicherzustellen, dass KI-Anwendungen dem Gemeinwohl dienen?</p>
+                        <h2>Artificial intelligence for sustainable development?</h2>
+                        <p>Artificial intelligence (AI) is currently in the limelight and raises many questions about economic policy, ethical, social and ecological expectations and implementation potential. At the first forum, Bits & Bäume, we discussed fundamental aspects: What can AI contribute to the transformation of society towards sustainability? What are the application examples that enable the use of AI for ecological or social objectives? What are policy options to ensure that AI applications serve the common good?</p>
                     </div>
                     <div>
                         <img style="max-width: 100%" class="portrait" src="/img/FBB1-fishbowl.jpg">
-                        <p><span class="bold">10. September 2019</span></p>
-                        <p>im Einstein Center Digital Future<br>ilhelmstraße 67, 10117 Berlin</p>
-                        <p class="info bold">Ein Diskussionspapier zur Veranstaltung wird derzeit noch um die Ergebnisse des ersten Forums angereichert und erscheint bald hier in aktualisierter Form.</p>
+                        <p><span class="bold">September 10th, 2019</span></p>
+                        <p>at the Einstein Center Digital Future<br>ilhelmstraße 67, 10117 Berlin</p>
+                        <p class="info bold">A discussion paper on the event is currently being enriched with the results of the first forum and will soon appear here in updated form.</p>
                     </div>
                 </div>
 
             </section>
 
             <section>
-                <p class="smaller">___<br>Das Forum Bits & Bäume wird von der wissenschaftlichen Forschungsgruppe „Digitalisierung und sozial-ökologische Transformation“ von IÖW und TU Berlin veranstaltet in Kooperation mit dem Einstein Center Digital Future und weiteren wechselnden Partnern. Das Projekt wird durch das Bundesministerium für Bildung und Forschung (BMBF) im Förderschwerpunkt sozial-ökologische Forschung (SÖF) gefördert.</p>
+                <p class="smaller">___<br>The Forum Bits & Bäume is organized by the scientific research group "Digitization and Sustainability" of the Institute for Ecological Economy Research and TU Berlin in cooperation with the Einstein Center Digital Future and other changing partners. The project is funded by the Federal Ministry of Education and Research (BMBF) in the Social-Ecological Research (SÖF) funding priority.</p>
             </section>
         </article>
 
