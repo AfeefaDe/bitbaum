f4336f5c7bd69d9e298d10568b7a35e356966b69
yuelika <32544557+yuelika@users.noreply.github.com>
2019-12-02 22:07:22 +0100

    moc Stammtische

--- public/components/sidebar.php
+++ public/components/sidebar.php
@@ -13,12 +13,6 @@
                     </p>
                 </article>
 
-                <article>
-                    <h3 class="bold">[Berlin, Dresden] <em>Bits&Bäume</em> regular's rounds</h3>
-                    <p>In <a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">Berlin</a> and <a href="https://dresden.bits-und-baeume.org" target="blank">Dresden</a>, the Bits&Bäume community now meets regularly to discuss, to keep up to date and to plan common events.
-                    </p>
-                </article>
-                
                  <article>
                     <h3 class="bold">[Leipzig, Dec 27-30] <em>Bits&Bäume</em>@Chaos Communication Congress</h3>
                     <p>This year's motto of the 36C3 is <a href="https://www.ccc.de/en/updates/2019/36c3-in-leipzig" target="blank"><em>Resource exhaustion</em>. </a> The program will be out soon, including talks by <em>Bits&Bäume</em> speakers and our legendary <em><a href="https://media.ccc.de/v/bub2018-249-sporangium#t=100" target="blank">Sporangium</a></em>!
@@ -62,11 +56,6 @@
                     <p>Über 2 Jahre hinweg diskutieren wir <a href="https://bits-und-baeume.org/forum/de" target="blank">in Expert*innenrunden die Gestaltung der Digitalisierung</a>. </p>
                 </article>
 
-                <article>
-                    <h3 class="bold">[Berlin, Dresden] Stammtische <em>Bits&Bäume</em></h3>
-                    <p>In <a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">Berlin</a> und <a href="https://dresden.bits-und-baeume.org" target="blank">Dresden</a> trifft sich regelmäßig die wachsende Bits&Bäume-Community, um zu diskutieren und gemeinsame Veranstaltungen zu planen.</p>
-                </article>
-                
                 <article>
                     <h3 class="bold">[Leipzig, 27.-30.12.] <em>Bits&Bäume</em>@Chaos Communication Congress</h3>
                     <p>Der 36C3 steht unter dem Motto <a href="https://www.ccc.de/en/updates/2019/36c3-in-leipzig" target="blank"><em>Resource exhaustion</em>. </a> Bald steht das Programm und wird euch mit einigen bekannten <em>Bits&Bäume</em>-Gesichtern überraschen – und einer Neuauflage des <em><a href="https://media.ccc.de/v/bub2018-249-sporangium#t=100" target="blank">Sporangiums</a></em>!
