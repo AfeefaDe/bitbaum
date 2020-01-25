03293a48c8160c3bbaaab7f366bd141f2c5af3f7
Mathias Renner <mail@mathias-renner.com>
2020-01-25 21:58:22 +0100

    Update description of local B&B platform in Berlin

--- public/pages/regionalzweige.php
+++ public/pages/regionalzweige.php
@@ -18,10 +18,10 @@ article .row--forderung > p {
 
   <article>
     <h1>Regional branches / round tables</h1>
-    
+
     <div class="row">
             <section>
-              <p> Die Bits&Bäume-Community grows! 
+              <p> Die Bits&Bäume-Community grows!
                 <br>You want to join? There are some first regional branches, meeting regularly for discussions or to plan talks, workshops or their own local conferences. Be part of our movement!
               </p>
             </section>
@@ -30,11 +30,11 @@ article .row--forderung > p {
               <li><a href="https://dresden.bits-und-baeume.org" target="blank">Bits & Bäume Dresden</a>
                 <br>Our first regular's round, initiated by the Faculty of Computer Science, TU Dresden, after two Bits&Bäume events on May 23rd and June 20th. Meeting at the University of Dresden.
               </li>
-              <li><a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">Bits & Bäume Berlin</a>
-                  <br>Regular's round, initiated by the ASta, TU Berlin. There are also workshops, and the Berlin branch set up our <a href="https://discourse.bits-und-baeume.org/" target="blank">online discussion board</a> – yay! They are meeting at the TU Berlin.
+              <li><a href="https://berlin.bits-und-baeume.org/" target="blank">Bits & Bäume Berlin</a>
+                  <br>B&B local platform, initiated by the ASta of TU Berlin. They organize events & workshops. The Berlin branch set up our <a href="https://discourse.bits-und-baeume.org/" target="blank">online discussion board</a> and an instance for <a href="mastodon.bits-und-baeume.org/">Mastodon</a> as an alternative to Twitter – yay!
               </li>
               <li><a href="http://bits-und-baeume-hannover.org/" target="blank">Bits & Bäume Hannover</a>
-                  <br>Regular's round, initiated by Chaos macht Schule. 
+                  <br>Regular's round, initiated by Chaos macht Schule.
               </li>
               <li><a href="/waechst/<?php echo $lang; ?>" target="blank">Who will be next? :)</a>
               </li>
@@ -43,8 +43,8 @@ article .row--forderung > p {
 
           <section>
             <h2>No Bits&Bäume nearby?</h2>
-                <p>Found your own regular's round! In our section <a href="/waechst/en<?php echo $lang; ?>" target="blank"><em> Bits&Bäume movement</em></a>, you can read more about what to do to organize your own Bits&Bäume formats. There is also a <a href="https://lists.posteo.de/listinfo/bitsundbaeume" target="blank">mailing list</a> as well as an <a href="https://discourse.bits-und-baeume.org/" target=blank">online discussion board</a>. 
-                </p>   
+                <p>Found your own regular's round! In our section <a href="/waechst/en<?php echo $lang; ?>" target="blank"><em> Bits&Bäume movement</em></a>, you can read more about what to do to organize your own Bits&Bäume formats. There is also a <a href="https://lists.posteo.de/listinfo/bitsundbaeume" target="blank">mailing list</a> as well as an <a href="https://discourse.bits-und-baeume.org/" target=blank">online discussion board</a>.
+                </p>
           </section>
   </article>
       <?php
@@ -56,7 +56,7 @@ break;
           <h1>Regionale Zweige und Stammtische</h1>
     <div class="row">
             <section>
-                <p> Die Bits&Bäume-Community wächst! 
+                <p> Die Bits&Bäume-Community wächst!
                   <br>Du willst mitmachen? In einigen Städten gibt es erste regionale Zweige mit eigenen Stammtischen zum Diskutieren und um zusammen Vorträge oder eigene Konferenzen zu planen. Schließ dich an!
                 </p>
             </section>
@@ -65,8 +65,8 @@ break;
                 <li><a href="https://dresden.bits-und-baeume.org" target="blank">Bits & Bäume Dresden</a>
                   <br>war der erste Stammtisch! :) Gegründet von der Fakultät Informatik der TU Dresden nach zwei eigenen Bits&Bäume-Veranstaltungen am 23. Mai und 20. Juni 2019. Trifft sich an der Uni Dresden.
                 </li>
-               <li><a href="https://asta.tu-berlin.de/aktuelles/bitsundb-ume-erster-stammtisch-berlin" target="blank">Bits & Bäume Berlin</a>
-                <br>B&B-Plattform gegründet vom AStA der TU Berlin, trifft sich – Achtung, Ortswechsel! – an der TU Berlin. Außerdem gibt es unregelmäßig verschiedene Workshops und die Berliner*innen haben das <a href="https://discourse.bits-und-baeume.org/" target=blank">Bits&Bäume-Online-Forum </a>aufgesetzt. Danke! :)  
+               <li><a href="https://berlin.bits-und-baeume.org/" target="blank">Bits & Bäume Berlin</a>
+                <br>Lokale Bits&Bäume Plattform gegründet vom AStA der TU Berlin. Es werden Veranstaltungen & Workshops organisiert. Die Berliner*innen haben das <a href="https://discourse.bits-und-baeume.org/" target=blank">Bits&Bäume-Online-Forum</a> und eine <a href="mastodon.bits-und-baeume.org/">Mastodon-Instanz als Twitter-Alternative</a> aufgesetzt. Danke! :)
               </li>
               <li><a href="http://bits-und-baeume-hannover.org/" target="blank">Bits & Bäume Hannover</a>
                   <br>entstanden aus einer regionalen Gruppe von <a href="https://ccc.de/schule" target="blank">Chaos macht Schule</a>. Trifft sich in den Räumen des CCC Hannover.
@@ -77,10 +77,10 @@ break;
           </div>
           <section>
             <h2>Noch kein Bits&Bäume-Sprössling in deiner Nähe?</h2>
-                <p>Dann gründe einen! Unter <a href="/waechst/<?php echo $lang; ?>" target="blank"><em> Bits&Bäume wächst</em></a> findest du die Bedingungen für eigene Bits&Bäume-Aktionen. Melde dich mit den guten Nachrichten, dass es Zuwachs gibt und wie mensch euch findet, unter <a href="mailto:bewegung@bits-und-baeume.org">bewegung@bits-und-baeume.org</a>, dann nehmen wir euch in die Liste auf. 
+                <p>Dann gründe einen! Unter <a href="/waechst/<?php echo $lang; ?>" target="blank"><em> Bits&Bäume wächst</em></a> findest du die Bedingungen für eigene Bits&Bäume-Aktionen. Melde dich mit den guten Nachrichten, dass es Zuwachs gibt und wie mensch euch findet, unter <a href="mailto:bewegung@bits-und-baeume.org">bewegung@bits-und-baeume.org</a>, dann nehmen wir euch in die Liste auf.
                 </p>
-                <p> Außerdem kannst du dich bei unserer <a href="https://lists.posteo.de/listinfo/bitsundbaeume" target="blank">Mailingliste</a> anmelden oder in unserem <a href="https://discourse.bits-und-baeume.org/" target=blank">Bits&Bäume-Online-Forum </a>aktiv werden und dich vernetzen. 
-              </p>   
+                <p> Außerdem kannst du dich bei unserer <a href="https://lists.posteo.de/listinfo/bitsundbaeume" target="blank">Mailingliste</a> anmelden oder in unserem <a href="https://discourse.bits-und-baeume.org/" target=blank">Bits&Bäume-Online-Forum </a>aktiv werden und dich vernetzen.
+              </p>
           </section>
 
         </article>
