3ce8841e97bb82640e9edc13c92e17fe09a5ebd4
Mathias Renner <mail@mathias-renner.com>
2020-03-22 16:55:41 +0100

    Talk session: update information

--- public/pages/kommunikations-tools.php
+++ public/pages/kommunikations-tools.php
@@ -67,16 +67,14 @@
                 </section>
 
                 <section>
-                    <p class="bold">Wann geht's los?</p>
+                    <p class="bold">Inhalte & Materialien</p>
                     <ul>
-                        <li>22. März 2020</li>
-			<li>16:30 - 18:00</li>
-			<li>Link zum Video kommt später...</li>
-			<li><a href="https://mathias-renner.com/dl/2020-03-22-Vortragsgespraech-Video-Text-Tools.mp3">Link zur Audiodatei</a></li>
-                        <li><a href="https://www.bits-und-baeume.org/downloads/2020-03-22-Vortragsgespraech-Video-Text-Tools.pdf">Folien</a></li>
-			<li><a href="https://conference.astatu.berlin/BitsUndBaeume">Jitsi Videochat-Root für Fragen</a>
-			<li><a href="https://cryptpad.fr/sheet/#/2/sheet/view/yc3J+xAsidHP+QeyiAf3DdD9HUG+ytM3Ek65X5WKfnk/embed/">Tabelle</a></li>
+                        <li><a href="https://tube.tchncs.de/videos/watch/6addc421-31f0-4126-be68-fe53e540d6a8" target="_blank">Vortragsgespräch als Video</a></li>
+                  			<li><a href="https://mathias-renner.com/dl/2020-03-22-Vortragsgespraech-Video-Text-Tools.mp3" target="_blank">Vortragsgespräch als Audio / Podcast</a></li>
+                        <li><a href="https://www.bits-und-baeume.org/downloads/2020-03-22-Vortragsgespraech-Video-Text-Tools.pdf" target="_blank">Folien / Präsentation</a></li>
+                  			<li><a href="https://cryptpad.fr/sheet/#/2/sheet/view/yc3J+xAsidHP+QeyiAf3DdD9HUG+ytM3Ek65X5WKfnk/embed/" target="_blank">Tabelle mit Detail-Informationen zu Video- und Text-Tools</a></li>
                     </ul>
+                    Alle Materialien unterliegen der <a href="https://creativecommons.org/licenses/by/2.0/de/">CC-BY</a> Lizenz.
                 </section>
             </div>
 
