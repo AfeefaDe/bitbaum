e71ab8671186b46c31faea788c50b90061d31ebb
Michael Winkelmann <michael.winkelmann@qt.io>
2020-03-22 17:00:41 +0100

    Changes from Moritz

--- public/pages/kommunikations-tools.php
+++ public/pages/kommunikations-tools.php
@@ -1,3 +1,4 @@
+ 
 <style>
     ul {
         list-style-type: "➤ ";
@@ -67,15 +68,22 @@
                 </section>
 
                 <section>
-                    <p class="bold">Wann geht's los?</p>
+                <p>Um sicherzustellen, dass wir bei der aktuellen Auslastung des Internets und einem schwer einzuschätzenden Andrang unsere Botschaft ohne Probleme vermitteln können, haben wir uns entschieden, statt des Livestreams ein vorab aufgezeichnetes Video zur Verfügung zu stellen. Wir ergänzen dieses durch einen Livechat, in dem wir auf Fragen antworten, sowie mit zusätzlichen Materialien.</p>
+                    <p class="bold">So geht's:</p>
+                    <ol>
+                        <li>Sieh dir unser Video (folgt) unten an, in dem wir die Bedeutung freier Software erklären und dir konkrete Vorschläge zu Software und Anbietern machen – es ist an jede*n gerichtet, ob du einen eigenen Server hast oder nicht.</li>
+                        <li>Besuche uns schon jetzt im Chat, wo wir euch auf Fragen gerne antworten.</li>
+                        <li>Sieh dir unsere Vortragsfolien und weitere Materialien an.</li>
+                    </ol>
+                </section>
+                <section>
+                    <p class="bold">Ressourcen</p>
                     <ul>
-                        <li>22. März 2020</li>
-			<li>16:30 - 18:00</li>
-			<li>Link zum Video kommt später...</li>
-			<li><a href="https://mathias-renner.com/dl/2020-03-22-Vortragsgespraech-Video-Text-Tools.mp3">Link zur Audiodatei</a></li>
-                        <li><a href="https://www.bits-und-baeume.org/downloads/2020-03-22-Vortragsgespraech-Video-Text-Tools.pdf">Folien</a></li>
-			<li><a href="https://conference.astatu.berlin/BitsUndBaeume">Jitsi Videochat-Root für Fragen</a>
-			<li><a href="https://cryptpad.fr/sheet/#/2/sheet/view/yc3J+xAsidHP+QeyiAf3DdD9HUG+ytM3Ek65X5WKfnk/embed/">Tabelle</a></li>
+                        <li>Das Video folgt in Kürze.</li>
+                        <li><a href="https://www.bits-und-baeume.org/downloads/2020-03-22-Vortragsgespraech-Video-Text-Tools.pdf">Die Vortragsfolien</a></li>
+                        <li><a href="https://conference.astatu.berlin/BitsUndBaeume">Zum Livechat</a></li>
+                        <li><a href="https://cryptpad.fr/sheet/#/2/sheet/view/yc3J+xAsidHP+QeyiAf3DdD9HUG+ytM3Ek65X5WKfnk/embed/">Eine Tabellenübersicht</a> mit zusätzlichen Informationen, auch zu anderen Tools.</li>
+                        <li><a href="https://mathias-renner.com/dl/2020-03-22-Vortragsgespraech-Video-Text-Tools.mp3">Eine Audioversion</a> des Vortrags als Podcast.</li>
                     </ul>
                 </section>
             </div>
