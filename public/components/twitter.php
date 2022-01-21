4bfd36063b1d7e0b56d7ef9d9f90a5bc7fff6591
AfeefaBot <kollektiv@afeefa.de>
2022-01-21 12:25:40 +0100

    fix twitter links

--- public/components/twitter.php
+++ public/components/twitter.php
@@ -8,7 +8,8 @@ $statuses = $tweets->statuses ?? [];
 
 foreach ($statuses as $key => $tweet) {
     if (substr($tweet->text, 0, 2) != "RT") {
-        $tweet_url = 'https://twitter.com/statuses/' . $tweet->id_str;
+        $user_url = 'https://twitter.com/' . $tweet->user->screen_name;
+        $tweet_url = $user_url . '/statuses/' . $tweet->id_str;
         // $date = date_create($tweet->created_at);
         $date = strtotime($tweet->created_at);
         ?>
@@ -20,8 +21,8 @@ foreach ($statuses as $key => $tweet) {
             <header>
                 <address>
                     <div>
-                        <?php echo $tweet->user->name ?>
-                        <small>@<?php echo $tweet->user->screen_name ?></small>
+                        <a href="<?php echo $user_url ?>" target="_blank"><?php echo $tweet->user->name ?></a>
+                        <small><a href="<?php echo $user_url ?>" target="_blank">@<?php echo $tweet->user->screen_name ?></a></small>
                     </div>
                 </address>
                 <time datetime="<?php echo date("Y-m-d H:i", $date) ?>">
