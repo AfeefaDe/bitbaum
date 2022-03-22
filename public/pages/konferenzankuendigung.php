e382510a16f76c5f86ece8285c3313f2c1049392
lynxblue <102161934+lynxblue@users.noreply.github.com>
2022-03-22 18:27:13 +0100

    Create konferenzankuendigung.php
    created ankuendigung on master

---
+++ public/pages/konferenzankuendigung.php
@@ -0,0 +1,124 @@
+<style>
+    article .row--forderung {
+        justify-content: flex-start;
+    }
+
+    article .row--forderung>span {
+        flex: 0 0 3em;
+        font-weight: bold;
+        text-indent: 0.8em;
+    }
+
+    article .row--forderung>p {
+        flex: 1 1 100%;
+        margin-top: 0;
+    }
+</style>
+<?php switch ($lang) {
+    case "en":
+?>
+
+        <article>
+            <h1><Bits & Bäume Conference 2022></h1>
+
+            <section>
+
+                <p>
+                Nach der erfolgreichen ersten Bits & Bäume-Konferenz 2018 sind die Themen Digitalisierung und Nachhaltigkeit heute in unserer Gesellschaft so präsent wie nie zuvor. Wir stehen vor großen gesellschaftlichen und globalen Herausforderungen für eine gerechte und nachhaltige Gestaltung unserer (Um-)Welt. Deshalb möchten wir mit der Konferenz Bits & Bäume 2022 vom 30. September bis 2. Oktober 2022 die Vernetzung von Umwelt- und Gerechtigkeitsaktivist*innen, Technikexpert*innen und Menschenrechtler*innen verbreitern und verstetigen. Dazu gehört auch die Vernetzung unserer Communities mit Vertreter*innen sozial-ökologisch wirtschaftender kleiner Digital-Unternehmen und -Gründer*innen sowie der Politik, um gemeinsam konkrete Lösungen für die drängenden Fragen unserer Zeit zu finden. Wir wollen große politische Strategien und Governance-Vorschläge diskutieren, aber auch anhand von Best-Practice-Beispielen voneinander lernen, wie wir im digitalen Zeitalter Klima- und Umweltschutz, soziale Gerechtigkeit, Demokratie und Grundrechte verwirklichen können. 
+                </p>
+
+                <p>
+                Gemeinsam wollen wir auf Podien, in Workshops und Open Spaces diskutieren:
+                </p>
+
+                <p>
+                
+                Wie kann die Digitalisierung so gestaltet werden, dass sie zu einer nachhaltigen Transformation der Gesellschaft beiträgt?
+            </p>
+            <section>
+                    <ul>
+                        <li>Wie sieht eine global, wirtschaftlich, sozial und ökologisch gerechte Zukunft in der digitalisierten Welt aus?
+                        </li>
+                       
+                    </ul>
+                    <ul>
+                        <li>Was können Tech-Community (Bits) und Gerechtigkeits- und Umweltbewegung (Bäume) voneinander lernen? Und wie können wir, auch gemeinsam mit progressiven und alternativ-ökonomischen Akteuren, politisch aktiv werden?
+                        </li>
+                       
+                    </ul>
+
+            </section>
+
+               
+            </section>
+            <section>
+                <h2>Wir brauchen eure Mithilfe!</h2>
+                <p>In einem Call for Action werden wir dazu  motivieren, sich in die Organisation der Konferenz einzubringen. Denn helfende Hände und Köpfe werden gebraucht. Es wird bunt, spannend und transformativ!
+</p>
+          
+            </section>
+            <section>
+                <h2>Eure Beiträge sind gefragt!
+</h2>
+                <p> Mit dem Call for Participation rufen wir Menschen aus zivilgesellschaftlichen Organisationen, progressive Akteur*innen aus der Wissenschaft, Arbeitswelt, Wirtschaft und Politik auf, die Konferenz durch eigene Beiträge mitzugestalten. Wir freuen uns auf vielfältige und kreative Themen und Formate!
+</p>
+             
+            </section>
+
+            
+        </article>
+    <?php
+        break;
+    default:
+    ?>
+        <article>
+
+           <h1>Bits & Bäume Konferenz 2022</h1>
+       
+            <section>
+
+                <p>
+                Nach der erfolgreichen ersten Bits & Bäume-Konferenz 2018 sind die Themen Digitalisierung und Nachhaltigkeit heute in unserer Gesellschaft so präsent wie nie zuvor. Wir stehen vor großen gesellschaftlichen und globalen Herausforderungen für eine gerechte und nachhaltige Gestaltung unserer (Um-)Welt. Deshalb möchten wir mit der Konferenz Bits & Bäume 2022 vom 30. September bis 2. Oktober 2022 die Vernetzung von Umwelt- und Gerechtigkeitsaktivist*innen, Technikexpert*innen und Menschenrechtler*innen verbreitern und verstetigen. Dazu gehört auch die Vernetzung unserer Communities mit Vertreter*innen sozial-ökologisch wirtschaftender kleiner Digital-Unternehmen und -Gründer*innen sowie der Politik, um gemeinsam konkrete Lösungen für die drängenden Fragen unserer Zeit zu finden. Wir wollen große politische Strategien und Governance-Vorschläge diskutieren, aber auch anhand von Best-Practice-Beispielen voneinander lernen, wie wir im digitalen Zeitalter Klima- und Umweltschutz, soziale Gerechtigkeit, Demokratie und Grundrechte verwirklichen können. 
+                </p>
+
+                <p>
+                Gemeinsam wollen wir auf Podien, in Workshops und Open Spaces diskutieren:
+                </p>
+
+                <p>
+                
+                Wie kann die Digitalisierung so gestaltet werden, dass sie zu einer nachhaltigen Transformation der Gesellschaft beiträgt?
+            </p>
+            <section>
+                    <ul>
+                        <li>Wie sieht eine global, wirtschaftlich, sozial und ökologisch gerechte Zukunft in der digitalisierten Welt aus?
+                        </li>
+                       
+                    </ul>
+                    <ul>
+                        <li>Was können Tech-Community (Bits) und Gerechtigkeits- und Umweltbewegung (Bäume) voneinander lernen? Und wie können wir, auch gemeinsam mit progressiven und alternativ-ökonomischen Akteuren, politisch aktiv werden?
+                        </li>
+                       
+                    </ul>
+
+            </section>
+
+               
+            </section>
+            <section>
+                <h2>Wir brauchen eure Mithilfe!</h2>
+                <p>In einem Call for Action werden wir dazu  motivieren, sich in die Organisation der Konferenz einzubringen. Denn helfende Hände und Köpfe werden gebraucht. Es wird bunt, spannend und transformativ!
+</p>
+          
+            </section>
+            <section>
+                <h2>Eure Beiträge sind gefragt!
+</h2>
+                <p> Mit dem Call for Participation rufen wir Menschen aus zivilgesellschaftlichen Organisationen, progressive Akteur*innen aus der Wissenschaft, Arbeitswelt, Wirtschaft und Politik auf, die Konferenz durch eigene Beiträge mitzugestalten. Wir freuen uns auf vielfältige und kreative Themen und Formate!
+</p>
+             
+            </section>
+
+                    
+        </article>
+<?php } ?>
