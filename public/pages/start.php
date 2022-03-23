f951ad1de76ba0acbdbf3ef443408762b8094925
lynxblue <102161934+lynxblue@users.noreply.github.com>
2022-03-23 09:18:10 +0100

    undo changes in start.php

--- public/pages/start.php
+++ public/pages/start.php
@@ -10,10 +10,10 @@
                 <h4><strong>Bits&Bäume becomes a movement</strong></h4>
 
                 <div class="row">
-                    <p class="square-action">
-                        <a href="/konferenzankuendigung/<?php echo $lang; ?>" class="button" style="text-align: center">Conference Bits&Bäume22</a>
-                  </p>
-                   
+                 
+                      <p class="square-action">
+                        <a href="/forum/<?php echo $lang; ?>" class="button" style="text-align: center">Forum Bits&Bäume</a>
+                 </p>
                     <p class="square-action">
                         <a href="/waechst/<?php echo $lang; ?>" class="button">organize B&B event</a>
                     </p>
@@ -23,9 +23,7 @@
                 <p class="square-action">
                    <a href="/regionalzweige/<?php echo $lang; ?>" class="button">local branches</a>
                 </p>
-                 <p class="square-action">
-                        <a href="/forum/<?php echo $lang; ?>" class="button" style="text-align: center">Forum Bits&Bäume</a>
-                 </p>
+              
                  
               </div>
 
@@ -65,9 +63,10 @@
                 <h4><strong>Bits&Bäume wächst zur Bewegung</strong></h4>
 
                 <div class="row">
+                     
                       <p class="square-action">
-                        <a href="/konferenzankuendigung/<?php echo $lang; ?>" class="button" style="text-align: center">Konferenz Bits&Bäume22</a>
-                  </p>
+                        <a href="/forum/<?php echo $lang; ?>" class="button" style="text-align: center">Forum Bits&Bäume</a>
+                    </p>
                    
                     <p class="square-action">
                         <a href="/waechst/<?php echo $lang; ?>" class="button">eigene B&B organisieren</a>
@@ -75,9 +74,7 @@
                 </div>
 
               <div class="row">
-                   <p class="square-action">
-                        <a href="/forum/<?php echo $lang; ?>" class="button" style="text-align: center">Forum Bits&Bäume</a>
-                    </p>
+                 
                 <p class="square-action">
                     <a href="/regionalzweige/<?php echo $lang; ?>" class="button">regionale Zweige</a>
                 </p>
