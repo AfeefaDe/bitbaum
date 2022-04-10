9c0373fc32215fa81886afa139a994511e2ffad2
Carsten Knoll <carsten.knoll@posteo.de>
2022-04-10 12:46:15 +0200

    remove blue box on right hand side (<aside>)

--- public/pages/konferenz.php
+++ public/pages/konferenz.php
@@ -22,6 +22,20 @@
 <script src="/js/perfect-scrollbar.min.js"></script>
 
 <script src="/js/KolleDeviceDetector.js"></script>
+
+
+
+<!-- Override some css rules for special case konferenz-->
+<style>
+    .sub-page header.main {
+        width: 100%;
+    }
+    body > section {
+        width: 100%;
+    }
+
+</style>
+
 </head>
 
 <body class="page-konferenz sub-page">
@@ -50,54 +64,6 @@
 
     </section>
 
-    <aside>
-
-            <!-- <section class="stats">
-            </section> -->
-
-            <section class="switch news active">
-                <article>
-                    <h3 class="bold">Komm dazu!</h3>
-                    <p>Um uns zu vernetzen, haben wir eine <a href="https://lists.posteo.de/listinfo/bitsundbaeume" target="_blank">Mailingliste</a>, einen <a href="https://twitter.com/bitsundbaeume/">Twitteraccount</a>, einen <a href="https://matrix.to/#/#bitsundbaeume:matrix.org" target="_blank">Community-Matrix-Chat</a>, ein <a href="https://discourse.bits-und-baeume.org" target="_blank">Community-Diskussionsbrett</a>, ein <a href="https://wiki.bits-und-baeume.org/struktur-der-bewegung/" target="_blank">Community-Wiki</a> und einen <a href="https://mastodon.bits-und-baeume.org/@bits_und_baeume" target="_blank">Community-Mastodon</a>.
-                    </p>
-                </article>
-
-                <article>
-                    <h3 class="bold">Aktuelle Termine</h3>
-                    <p>Unser aktueller Terminkalender ist <a href="https://discourse.bits-und-baeume.org/calendar" target="_blank">hier</a>.</p>
-                </article>
-
-                <article>
-                    <h3 class="bold">Buch: <em>Was Bits&Bäume verbindet</em></h3>
-                    <p>Das <a href="https://www.oekom.de/nc/buecher/gesamtprogramm/buch/was-bits-baeume-verbindet.html" target="_blank">Buch zur Konferenz</a> ist da. Dank an die successfully fundende Crowd!</p>
-                </article>
-
-                <article>
-                    <h3 class="bold">Berliner Gesprächsreihe <em>Forum Bits&Bäume</em></h3>
-                    <p>Über 2 Jahre hinweg diskutieren wir <a href="/forum/de">in Expert*innenrunden die Gestaltung der Digitalisierung</a>.</p>
-                </article>
-            </section>
-
-            <section class="switch twitter">
-
-        <article>
-            Hier würden Tweets stehen.
-        </article>
-
-            </section>
-
-            <div class="control">
-                <div class="news active">
-                    <div class="icon"></div>
-                    <p>Aktuelles</p>
-                </div>
-                <div class="twitter">
-                    <div class="icon"></div>
-                    <p>#bitsundbäume</p>
-                </div>
-            </div>
-
-</aside>
 
 <script>
     if (!KolleDeviceDetector.isMobile()) {
